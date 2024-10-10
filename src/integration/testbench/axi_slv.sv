54dfbbc39fe0db54ae793e314c9ed02c0324ed9c
Caleb Whitehead <cwhitehead@microsoft.com>
2024-10-09 20:25:17 -0700

    Use aligned addresses for indexing memory

--- src/integration/testbench/axi_slv.sv
+++ src/integration/testbench/axi_slv.sv
@@ -43,6 +43,12 @@ module axi_slv #(
   bit [7:0] mem[bit [31:0]];
   bit [63:0] memdata;
   bit        rlast_local;
+  logic [31:0]            awaddr_aligned;
+  logic [31:0]            araddr_aligned;
+  always_comb begin
+      awaddr_aligned = {awaddr[31:3],3'b000};
+      araddr_aligned = {araddr[31:3],3'b000};
+  end
 
   reg [TAGW-1:0] awid_local;
 
@@ -67,29 +73,29 @@ module axi_slv #(
 
     if (arvalid) begin
       memdata <= {
-        mem[araddr+7],
-        mem[araddr+6],
-        mem[araddr+5],
-        mem[araddr+4],
-        mem[araddr+3],
-        mem[araddr+2],
-        mem[araddr+1],
-        mem[araddr]
+        mem[araddr_aligned+7],
+        mem[araddr_aligned+6],
+        mem[araddr_aligned+5],
+        mem[araddr_aligned+4],
+        mem[araddr_aligned+3],
+        mem[araddr_aligned+2],
+        mem[araddr_aligned+1],
+        mem[araddr_aligned]
       };
       rlast_local  <= 1'b1;
     end else begin
       rlast_local  <= 1'b0;
     end
 
-    if (awvalid) begin
-      if (wstrb[7]) mem[awaddr+7] = wdata[63:56];
-      if (wstrb[6]) mem[awaddr+6] = wdata[55:48];
-      if (wstrb[5]) mem[awaddr+5] = wdata[47:40];
-      if (wstrb[4]) mem[awaddr+4] = wdata[39:32];
-      if (wstrb[3]) mem[awaddr+3] = wdata[31:24];
-      if (wstrb[2]) mem[awaddr+2] = wdata[23:16];
-      if (wstrb[1]) mem[awaddr+1] = wdata[15:08];
-      if (wstrb[0]) mem[awaddr+0] = wdata[07:00];
+    if(awvalid) begin
+        if(wstrb[7]) mem[awaddr_aligned+7] = wdata[63:56];
+        if(wstrb[6]) mem[awaddr_aligned+6] = wdata[55:48];
+        if(wstrb[5]) mem[awaddr_aligned+5] = wdata[47:40];
+        if(wstrb[4]) mem[awaddr_aligned+4] = wdata[39:32];
+        if(wstrb[3]) mem[awaddr_aligned+3] = wdata[31:24];
+        if(wstrb[2]) mem[awaddr_aligned+2] = wdata[23:16];
+        if(wstrb[1]) mem[awaddr_aligned+1] = wdata[15:08];
+        if(wstrb[0]) mem[awaddr_aligned+0] = wdata[07:00];
     end
   end
 
