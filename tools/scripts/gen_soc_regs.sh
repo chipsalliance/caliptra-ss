5c1733efcf573a7824cdb89b73153310e56b48ce
Caleb Whitehead <cwhitehead@microsoft.com>
2024-10-22 15:53:52 -0700

    Script to generate soc addr map

---
+++ tools/scripts/gen_soc_regs.sh
@@ -0,0 +1,6 @@
+python3 ${CALIPTRA_ROOT}/tools/scripts/reg_doc_gen.py \
+${CALIPTRA_SS}/src/integration/rtl/soc_address_map.rdl \
+${CALIPTRA_ROOT}/src/soc_ifc/rtl/mbox_csr.rdl \
+${CALIPTRA_ROOT}/src/soc_ifc/rtl/sha512_acc_csr.rdl \
+${CALIPTRA_ROOT}/src/soc_ifc/rtl/soc_ifc_doc.rdl \
+${I3C_ROOT}/src/rdl/registers.rdl
