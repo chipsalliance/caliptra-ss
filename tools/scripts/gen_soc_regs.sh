b1dfb8bb1eda48783c61e1c5f463cfd9404b6316
Caleb Whitehead <cwhitehead@microsoft.com>
2024-11-05 08:59:13 -0800

    Use submodule paths for rdl gen

--- tools/scripts/gen_soc_regs.sh
+++ tools/scripts/gen_soc_regs.sh
@@ -1,6 +1,6 @@
-python3 ${CALIPTRA_ROOT}/tools/scripts/reg_doc_gen.py \
+python3 ${CALIPTRA_SS}/third_party/caliptra-rtl/tools/scripts/reg_doc_gen.py \
 ${CALIPTRA_SS}/src/integration/rtl/soc_address_map.rdl \
-${CALIPTRA_ROOT}/src/soc_ifc/rtl/mbox_csr.rdl \
-${CALIPTRA_ROOT}/src/soc_ifc/rtl/sha512_acc_csr.rdl \
-${CALIPTRA_ROOT}/src/soc_ifc/rtl/soc_ifc_doc.rdl \
-${I3C_ROOT}/src/rdl/registers.rdl
+${CALIPTRA_SS}/third_party/caliptra-rtl/src/soc_ifc/rtl/mbox_csr.rdl \
+${CALIPTRA_SS}/third_party/caliptra-rtl/src/soc_ifc/rtl/sha512_acc_csr.rdl \
+${CALIPTRA_SS}/third_party/caliptra-rtl/src/soc_ifc/rtl/soc_ifc_doc.rdl \
+${CALIPTRA_SS}/third_party/i3c-core/src/rdl/registers.rdl
