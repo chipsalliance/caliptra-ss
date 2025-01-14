163d51a6beb7d5bd0476b6df461d2fcc863fb20a
Michael Norris <michnorris@microsoft.com>
2025-01-14 14:33:23 -0800

    fixing commented out params

--- src/mci/rtl/mci_axi_sub_top.sv
+++ src/mci/rtl/mci_axi_sub_top.sv
@@ -64,10 +64,10 @@ module mci_axi_sub_top
 
     );
 
-localparam AXI_ADDR_WIDTH = 32;//s_axi_w_if.AW;
-localparam AXI_DATA_WIDTH = 32;//s_axi_w_if.DW;
-localparam AXI_USER_WIDTH = 32;//s_axi_w_if.UW;
-localparam AXI_ID_WIDTH   = 8;//s_axi_w_if.IW;
+localparam AXI_ADDR_WIDTH = s_axi_w_if.AW;
+localparam AXI_DATA_WIDTH = s_axi_w_if.DW;
+localparam AXI_USER_WIDTH = s_axi_w_if.UW;
+localparam AXI_ID_WIDTH   = s_axi_w_if.IW;
 
 // Interface between axi_sub and mci decoder
 cif_if #(
