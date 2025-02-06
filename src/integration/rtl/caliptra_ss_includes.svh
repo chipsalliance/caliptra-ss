d1a5078fdf5d28d8ef5a40a5637e71531f56bc8d
Emre Karabulut <emre.karabulut.dde@gmail.com>
2025-02-06 15:35:10 -0800

    table json file updated with new strap names

--- src/integration/rtl/caliptra_ss_includes.svh
+++ src/integration/rtl/caliptra_ss_includes.svh
@@ -5,7 +5,7 @@
 
 parameter CPTRA_SS_MCU_USER_WIDTH = 32;
 
-parameter CALIPTRA_CORE_AXI_USER_ID =          32'h0;
-parameter MCU_AXI_USER_ID           =          32'h1;
+parameter CPTRA_SS_STRAP_CLPTRA_CORE_AXI_USER   =          32'h0;
+parameter CPTRA_SS_STRAP_MCU_LSU_AXI_USER       =          32'h1;
 
 `endif // CPTRA_SS_INCLUDES_SVH
