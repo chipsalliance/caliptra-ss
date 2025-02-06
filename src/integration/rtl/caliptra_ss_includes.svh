9beedba5fe0602291f7189159c18fccb675ac3d9
Emre Karabulut <emre.karabulut.dde@gmail.com>
2025-02-06 13:45:37 -0800

    access control table for fuse ctrl

--- src/integration/rtl/caliptra_ss_includes.svh
+++ src/integration/rtl/caliptra_ss_includes.svh
@@ -5,4 +5,7 @@
 
 parameter CPTRA_SS_MCU_USER_WIDTH = 32;
 
+parameter CALIPTRA_CORE_AXI_USER_ID =          32'h0;
+parameter MCU_AXI_USER_ID           =          32'h1;
+
 `endif // CPTRA_SS_INCLUDES_SVH
