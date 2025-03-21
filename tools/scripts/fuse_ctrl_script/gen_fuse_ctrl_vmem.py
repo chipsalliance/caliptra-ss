cf79e0c909f851e0ac01a25e7d7d31128a63168a
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-03-21 05:38:36 -0700

    [fuse_ctrl, script] Add VMEM generation script
    This commit adds a script that generated VMEM files that can be loaded
    into the fuse controller. The life cycle state, counter, and the
    transition token can be programed by using cmd line arguments.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

---
+++ tools/scripts/fuse_ctrl_script/gen_fuse_ctrl_vmem.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python3
+# Copyright lowRISC contributors (OpenTitan project).
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+r"""Generate RTL and documentation collateral from OTP memory
+map definition file (hjson).
+"""
+import argparse
+import datetime
+import logging as log
+import random
+from pathlib import Path
+from Crypto.Hash import cSHAKE128
+
+import hjson
+
+from lib.common import wrapped_docstring
+from lib.otp_mem_img import OtpMemImg
+from lib.otp_mem_map import OtpMemMap
+
+
+# Default output path (can be overridden on the command line). Note that
+# "BITWIDTH" will be replaced with the architecture's bitness.
+MEMORY_MEM_FILE = 'otp-img.BITWIDTH.vmem'
+
+
+def _override_seed(args, seed_name, config):
+    '''Override the seed key in config with value specified in args'''
+    arg_seed = getattr(args, seed_name)
+
+    # An override seed of 0 will not trigger the override, which is intended, as
+    # the OTP-generation Bazel rule sets the default seed values to 0.
+    if arg_seed:
+        log.warning('Commandline override of {} with {}.'.format(
+            seed_name, arg_seed))
+        config['seed'] = arg_seed
+    # Otherwise, we either take it from the .hjson if present, or
+    # randomly generate a new seed if not.
+    else:
+        new_seed = random.getrandbits(256)
+        if config.setdefault('seed', new_seed) == new_seed:
+            log.warning('No {} specified, setting to {}.'.format(
+                seed_name, new_seed))
+
+
+def main():
+    log.basicConfig(level=log.INFO, format="%(levelname)s: %(message)s")
+
+    parser = argparse.ArgumentParser(
+        prog="gen-otp-img",
+        description=wrapped_docstring(),
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-stamp',
+                        action='store_true',
+                        help='''
+                        Add a comment 'Generated on [Date] with [Command]' to
+                        generated output files.
+                        ''')
+    parser.add_argument('--otp-seed',
+                        type=int,
+                        metavar='<seed>',
+                        help='''
+                        Custom seed for RNG to compute randomized OTP netlist constants.
+
+                        Note that this seed must coincide with the seed used for generating
+                        the OTP memory map (gen-otp-mmap.py).
+
+                        This value typically does not need to be specified as it is taken from
+                        the OTP memory map definition Hjson.
+                        ''')
+    parser.add_argument('-o',
+                        '--out',
+                        type=str,
+                        metavar='<path>',
+                        default=MEMORY_MEM_FILE,
+                        help='''
+                        Custom output path for generated MEM file.
+                        Defaults to {}
+                        '''.format(MEMORY_MEM_FILE))
+    parser.add_argument('--lc-state-def',
+                        type=Path,
+                        metavar='<path>',
+                        required=True,
+                        help='''
+                        Life cycle state definition file in Hjson format.
+                        ''')
+    parser.add_argument('--mmap-def',
+                        type=Path,
+                        metavar='<path>',
+                        required=True,
+                        help='''
+                        Path to OTP memory map file in Hjson format.
+                        ''')
+    parser.add_argument('--lc-state',
+                        type=str,
+                        metavar='state',
+                        required=True,
+                        help='''
+                        Life cycle state to write into the OTP.
+                        ''')
+    parser.add_argument('--lc-cnt',
+                        type=int,
+                        metavar='counter',
+                        required=True,
+                        help='''
+                        Life cycle counter to write into the OTP.
+                        ''')
+    parser.add_argument('--token-name',
+                        type=str,
+                        metavar='token-name',
+                        required=False,
+                        help='''
+                        Name of the token to write into the OTP.
+                        ''')
+    parser.add_argument('--token-value',
+                        type=str,
+                        metavar='token-value',
+                        required=False,
+                        help='''
+                        Raw token value to write into the OTP.
+                        ''')
+
+    args = parser.parse_args()
+
+    log.info('Loading LC state definition file {}'.format(args.lc_state_def))
+    with open(args.lc_state_def, 'r') as infile:
+        lc_state_cfg = hjson.load(infile)
+    log.info('Loading OTP memory map definition file {}'.format(args.mmap_def))
+    with open(args.mmap_def, 'r') as infile:
+        otp_mmap_cfg = hjson.load(infile)
+
+     # If specified, override the seed.
+    _override_seed(args, 'otp_seed', otp_mmap_cfg)
+
+    img_config = {}
+    img_config['seed'] = 0 # Not used.
+    img_config['partitions'] = []
+    # Configure the LC state & counter.
+    lc_config = {}
+    lc_config['name'] = 'LIFE_CYCLE'
+    lc_config['count'] = args.lc_cnt
+    lc_config['state'] = args.lc_state
+    lc_config['items'] = []
+    lc_config['lock'] = False
+    img_config['partitions'].append(lc_config)
+    # Configure the unlock token.
+    if args.token_name is not None and args.token_value is not None:
+        # Hash the token.
+        value = int(args.token_value, 0)
+        data = value.to_bytes(16, byteorder='little')
+        custom = 'LC_CTRL'.encode('UTF-8')
+        shake = cSHAKE128.new(data=data, custom=custom)
+        digest = int.from_bytes(shake.read(16), byteorder='little')
+        # Configure the partition.
+        token_config = {}
+        token_config['name'] = 'SECRET_LC_TRANSITION_PARTITION'
+        token_config['lock'] = True # Lock the partition such that the digest is calculated.
+        token_item = {}
+        token_item['name'] = args.token_name
+        token_item['value'] = str(hex(digest))
+        token_config['items'] = []
+        token_config['items'].append(token_item)
+        img_config['partitions'].append(token_config)
+
+    try:
+        otp_mem_img = OtpMemImg(lc_state_cfg, otp_mmap_cfg, img_config, '')
+
+    except RuntimeError as err:
+        log.error(err)
+        exit(1)
+
+    # Print all defined args into header comment for reference
+    argstr = ''
+    for arg, argval in sorted(vars(args).items()):
+        if argval:
+            if not isinstance(argval, list):
+                argval = [argval]
+            for a in argval:
+                argname = '-'.join(arg.split('_'))
+                # Get absolute paths for all files specified.
+                a = a.resolve() if isinstance(a, Path) else a
+                argstr += ' \\\n//   --' + argname + ' ' + str(a) + ''
+
+    file_header = '//\n'
+    if args.stamp:
+        dt = datetime.datetime.now(datetime.timezone.utc)
+        dtstr = dt.strftime("%a, %d %b %Y %H:%M:%S %Z")
+        file_header = '// Generated on {} with\n// $ gen-otp-img.py {}\n//\n'.format(
+            dtstr, argstr)
+
+    memfile_body, bitness = otp_mem_img.streamout_memfile()
+
+    # If the out argument does not contain "BITWIDTH", it will not be changed.
+    memfile_path = Path(args.out.replace('BITWIDTH', str(bitness)))
+
+    # Use binary mode and a large buffer size to improve performance.
+    with open(memfile_path, 'wb', buffering=2097152) as outfile:
+        outfile.write(file_header.encode('utf-8'))
+        outfile.write(memfile_body.encode('utf-8'))
+
+
+if __name__ == "__main__":
+    main()
