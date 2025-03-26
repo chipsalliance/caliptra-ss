58b9832c7117c3f100b45edc961481a24d7542ba
Caleb Whitehead <cwhitehead@microsoft.com>
2025-03-26 11:41:04 -0700

    Replace instances of the deprecated var CALIPTRA_SS with CALIPTRA_SS_ROOT (removes duplicate variables)

--- tools/scripts/gen_caliptra_ss_regression_ymls.py
+++ tools/scripts/gen_caliptra_ss_regression_ymls.py
@@ -398,10 +398,10 @@ def scan_test_suites(caliptra_ss):
 
 
 if __name__ == "__main__":
-    caliptra_ss = os.getenv('CALIPTRA_SS')
+    caliptra_ss = os.getenv('CALIPTRA_SS_ROOT')
 
     if not caliptra_ss:
-        logger.error("CALIPTRA_SS environment variable is not set. Please set it before running this script.")
+        logger.error("CALIPTRA_SS_ROOT environment variable is not set. Please set it before running this script.")
         exit(1)
     
     # Scan for all test suites first
