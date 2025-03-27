2d9f5a0e80dd2d51be048a51e6b7bf7db76e34d4
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-03-27 05:27:33 -0700

    [lc_ctrl, test] Randomly generate unlock tokens
    This commit extends the test run script such that the
    gen_fuse_ctrl_vmem.py script is used to randomize the
    state transition unlock tokens.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- src/integration/test_suites/smoke_test_lcc_kmac_kat/smoke_test_lcc_kmac_kat.c
+++ src/integration/test_suites/smoke_test_lcc_kmac_kat/smoke_test_lcc_kmac_kat.c
@@ -12,6 +12,8 @@
 #include "caliptra_ss_lib.h"
 #include "fuse_ctrl.h"
 #include "lc_ctrl.h"
+// Auto generated with the gen_fuse_ctrl_vmem script.
+#include "smoke_test_lcc_kmac_kat.h"
 
 volatile char* stdout = (char *)SOC_MCI_TOP_MCI_REG_DEBUG_OUT;
 #ifdef CPT_VERBOSITY
@@ -32,31 +34,6 @@ void main (void) {
 
     uint32_t cptra_boot_go = lsu_read_32(SOC_MCI_TOP_MCI_REG_CPTRA_BOOT_GO);
     VPRINTF(LOW, "MCU: Reading SOC_MCI_TOP_MCI_REG_CPTRA_BOOT_GO %x\n", cptra_boot_go);
-    
-    // Raw (unhashed) transition tokens.
-    const uint32_t tokens[NUM_STATES][4] = {
-        {0x00000000, 0x00000000, 0x00000000, 0x00000000}, // empty
-        {0x00000000, 0x00000000, 0x00000000, 0x00000000}, // empty
-        {0x00000000, 0x00000000, 0x00000000, 0x00000000}, // empty
-        {0x7EBEA08B, 0x44593938, 0xB6F4A7E9, 0x16F6064B}, // CPTRA_SS_TEST_UNLOCK_TOKEN_0
-        {0x00000000, 0x00000000, 0x00000000, 0x00000000}, // empty
-        {0x8757DF07, 0x3FAC7019, 0x452ED1A3, 0x3C4353DC}, // CPTRA_SS_TEST_UNLOCK_TOKEN_1
-        {0x00000000, 0x00000000, 0x00000000, 0x00000000}, // empty
-        {0x7E9F2B57, 0x66696701, 0x352CFAAA, 0x4F005CE5}, // CPTRA_SS_TEST_UNLOCK_TOKEN_2
-        {0x00000000, 0x00000000, 0x00000000, 0x00000000}, // empty
-        {0x630BBA80, 0xF8C528CD, 0x0E4A5DCD, 0xC7050FC4}, // CPTRA_SS_TEST_UNLOCK_TOKEN_3
-        {0x00000000, 0x00000000, 0x00000000, 0x00000000}, // empty
-        {0xF2F130D7, 0xC5B2F9C0, 0xF254BBF6, 0x9D30EC03}, // CPTRA_SS_TEST_UNLOCK_TOKEN_4
-        {0x00000000, 0x00000000, 0x00000000, 0x00000000}, // empty
-        {0x71210A41, 0x56B5A675, 0xD535CDB7, 0x43272B5A}, // CPTRA_SS_TEST_UNLOCK_TOKEN_5
-        {0x00000000, 0x00000000, 0x00000000, 0x00000000}, // empty
-        {0xBD963661, 0xCB08EF49, 0x8ADEBE61, 0x830048CF}, // CPTRA_SS_TEST_UNLOCK_TOKEN_6
-        {0x571D77F2, 0x1A14A21A, 0x60E3AE2A, 0x1EECAD58}, // CPTRA_SS_TEST_EXIT_TO_MANUF_TOKEN
-        {0xCDD74C7A, 0x0408CDD4, 0x255AB47C, 0xFDA4DF39}, // CPTRA_SS_MANUF_TO_PROD_TOKEN
-        {0x11DDBC6F, 0x2B2A890A, 0x6B49F203, 0xC0B77295}, // CPTRA_SS_PROD_TO_PROD_END_TOKEN
-        {0x00000000, 0x00000000, 0x00000000, 0x00000000}, // empty
-        {0x00000000, 0x00000000, 0x00000000, 0x00000000}  // empty
-    };
 
     lcc_initialization();
     uint32_t lc_state_curr = read_lc_state();
