3dfc30944e52d388de3bf5fd078cc2d3e129310f
Andrea Caforio <andrea.caforio@lowrisc.org>
2025-04-12 11:08:20 -0700

    [fuse_ctrl, cov] Token coverage
    Signed-off-by: Andrea Caforio <andrea.caforio@lowrisc.org>

--- src/fuse_ctrl/coverage/fuse_ctrl_cov_if.sv
+++ src/fuse_ctrl/coverage/fuse_ctrl_cov_if.sv
@@ -25,6 +25,7 @@ interface fuse_ctrl_cov_if
     import axi_struct_pkg::*;
     import otp_ctrl_reg_pkg::*;
     import otp_ctrl_pkg::*;
+    import lc_ctrl_state_pkg::*;
 
     /** fuse_ctrl_filter:
      *
@@ -84,6 +85,137 @@ interface fuse_ctrl_cov_if
     `FUSE_CG(CptraCoreFieldEntropy0Offset, CptraCoreFieldEntropy0Size)
     `FUSE_CG(SecretProdPartition0DigestOffset, SecretProdPartition0DigestSize)
     // TODO: fill all fuses
+
+    /** fuse_ctrl test unlock tokens:
+     *
+     *  Verify that all test unlock tokens are broadcasted and transitions are correct.
+     */
+
+    logic [31:0] test_unlock_token_idx;
+    assign test_unlock_token_idx = `FC_PATH.test_unlock_token_idx;
+
+    lc_token_t test_unlock_token;
+    lc_token_t test_exit_dev_token;
+    lc_token_t dev_exit_prod_token;
+    lc_token_t prod_exit_prodend_token;
+    assign test_unlock_token       = `FC_PATH.otp_lc_data_o.test_unlock_token;
+    assign test_exit_dev_token     = `FC_PATH.otp_lc_data_o.test_exit_dev_token;
+    assign dev_exit_prod_token     = `FC_PATH.otp_lc_data_o.dev_exit_prod_token;
+    assign prod_exit_prodend_token = `FC_PATH.otp_lc_data_o.prod_exit_prodend_token;
+
+    covergroup fuse_ctrl_test_unlock_tokens_cg @(posedge clk_i);
+        option.per_instance = 1;
+
+        fuse_ctrl_test_unlock_token_idx_cp: coverpoint test_unlock_token_idx
+        {
+            bins CptraSsTestUnlockTokenOffsets[] = {
+                CptraSsTestUnlockToken0Offset,
+                CptraSsTestUnlockToken1Offset,
+                CptraSsTestUnlockToken2Offset,
+                CptraSsTestUnlockToken3Offset,
+                CptraSsTestUnlockToken4Offset,
+                CptraSsTestUnlockToken5Offset,
+                CptraSsTestUnlockToken6Offset,
+                '0
+            };
+        }
+
+        fuse_ctrl_test_unlock_token_transitions_cp: coverpoint test_unlock_token_idx
+        {
+            bins CptraSsTestUnlockToken0Transitions = ( 
+                CptraSsTestUnlockToken0Offset => '0 => CptraSsTestUnlockToken1Offset,
+                CptraSsTestUnlockToken0Offset => '0 => CptraSsTestUnlockToken2Offset,
+                CptraSsTestUnlockToken0Offset => '0 => CptraSsTestUnlockToken3Offset,
+                CptraSsTestUnlockToken0Offset => '0 => CptraSsTestUnlockToken4Offset,
+                CptraSsTestUnlockToken0Offset => '0 => CptraSsTestUnlockToken5Offset,
+                CptraSsTestUnlockToken0Offset => '0 => CptraSsTestUnlockToken6Offset
+            );
+            bins CptraSsTestUnlockToken1Transitions = (
+                CptraSsTestUnlockToken1Offset => '0 => CptraSsTestUnlockToken2Offset,
+                CptraSsTestUnlockToken1Offset => '0 => CptraSsTestUnlockToken3Offset,
+                CptraSsTestUnlockToken1Offset => '0 => CptraSsTestUnlockToken4Offset,
+                CptraSsTestUnlockToken1Offset => '0 => CptraSsTestUnlockToken5Offset,
+                CptraSsTestUnlockToken1Offset => '0 => CptraSsTestUnlockToken6Offset
+            );
+            bins CptraSsTestUnlockToken2Transitions = ( 
+                CptraSsTestUnlockToken2Offset => '0 => CptraSsTestUnlockToken3Offset,
+                CptraSsTestUnlockToken2Offset => '0 => CptraSsTestUnlockToken4Offset,
+                CptraSsTestUnlockToken2Offset => '0 => CptraSsTestUnlockToken5Offset,
+                CptraSsTestUnlockToken2Offset => '0 => CptraSsTestUnlockToken6Offset
+            );
+            bins CptraSsTestUnlockToken3Transitions = ( 
+                CptraSsTestUnlockToken3Offset => '0 => CptraSsTestUnlockToken4Offset,
+                CptraSsTestUnlockToken3Offset => '0 => CptraSsTestUnlockToken5Offset,
+                CptraSsTestUnlockToken3Offset => '0 => CptraSsTestUnlockToken6Offset
+            );
+            bins CptraSsTestUnlockToken4Transitions = ( 
+                CptraSsTestUnlockToken4Offset => '0 => CptraSsTestUnlockToken5Offset,
+                CptraSsTestUnlockToken4Offset => '0 => CptraSsTestUnlockToken6Offset
+            );
+            bins CptraSsTestUnlockToken5Transitions = ( 
+                CptraSsTestUnlockToken5Offset => '0 => CptraSsTestUnlockToken6Offset
+            );
+
+            illegal_bins CptraSsTestUnlockToken1IllegalTransitions = (
+                CptraSsTestUnlockToken1Offset => '0 => CptraSsTestUnlockToken0Offset
+            );
+
+            illegal_bins CptraSsTestUnlockToken2IllegalTransitions = (
+                CptraSsTestUnlockToken2Offset => '0 => CptraSsTestUnlockToken0Offset,
+                CptraSsTestUnlockToken2Offset => '0 => CptraSsTestUnlockToken1Offset
+            );
+
+            illegal_bins CptraSsTestUnlockToken3IllegalTransitions = (
+                CptraSsTestUnlockToken3Offset => '0 => CptraSsTestUnlockToken0Offset,
+                CptraSsTestUnlockToken3Offset => '0 => CptraSsTestUnlockToken1Offset,
+                CptraSsTestUnlockToken3Offset => '0 => CptraSsTestUnlockToken2Offset
+            );
+
+            illegal_bins CptraSsTestUnlockToken4IllegalTransitions = (
+                CptraSsTestUnlockToken4Offset => '0 => CptraSsTestUnlockToken0Offset,
+                CptraSsTestUnlockToken4Offset => '0 => CptraSsTestUnlockToken1Offset,
+                CptraSsTestUnlockToken4Offset => '0 => CptraSsTestUnlockToken2Offset,
+                CptraSsTestUnlockToken4Offset => '0 => CptraSsTestUnlockToken3Offset
+            );
+
+            illegal_bins CptraSsTestUnlockToken5IllegalTransitions = (
+                CptraSsTestUnlockToken5Offset => '0 => CptraSsTestUnlockToken0Offset,
+                CptraSsTestUnlockToken5Offset => '0 => CptraSsTestUnlockToken1Offset,
+                CptraSsTestUnlockToken5Offset => '0 => CptraSsTestUnlockToken2Offset,
+                CptraSsTestUnlockToken5Offset => '0 => CptraSsTestUnlockToken3Offset,
+                CptraSsTestUnlockToken5Offset => '0 => CptraSsTestUnlockToken4Offset
+            );
+
+            illegal_bins CptraSsTestUnlockToken6IllegalTransitions = (
+                CptraSsTestUnlockToken6Offset => '0 => CptraSsTestUnlockToken0Offset,
+                CptraSsTestUnlockToken6Offset => '0 => CptraSsTestUnlockToken1Offset,
+                CptraSsTestUnlockToken6Offset => '0 => CptraSsTestUnlockToken2Offset,
+                CptraSsTestUnlockToken6Offset => '0 => CptraSsTestUnlockToken3Offset,
+                CptraSsTestUnlockToken6Offset => '0 => CptraSsTestUnlockToken4Offset,
+                CptraSsTestUnlockToken6Offset => '0 => CptraSsTestUnlockToken5Offset
+            );           
+        }
+
+        fuse_ctrl_test_unlock_token_cp: coverpoint test_unlock_token
+        {
+            bins TestUnlockToken = { [1:] };
+        }
+        fuse_ctrl_test_exit_dev_token_cp: coverpoint test_exit_dev_token
+        {
+            bins TestExitDevToken = { [1:] };
+        }
+        fuse_ctrl_dev_exit_prod_token_cp: coverpoint dev_exit_prod_token
+        {
+            bins DevExitProdToken = { [1:] };
+        }
+        fuse_ctrl_prod_exit_prodend_token_cp: coverpoint prod_exit_prodend_token
+        {
+            bins ProdExitProdendToken = { [1:] };
+        }
+
+    endgroup
+
+
 
 endinterface
 
