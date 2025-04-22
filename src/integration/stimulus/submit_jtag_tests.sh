b2c84bd319cb3f4c7b1359fa3edc6a62226235dc
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-04-22 05:22:56 -0700

    [lcc, test] Extend caliptra_ss_jtag_lcc_st_trans
    This commit extends the caliptra_ss_jtag_lcc_st_trans test.
    - The test starts at a random LC state & counter
    - Then, based on the transition matrix, it does several
      state transitions until SCRAP is reached.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- src/integration/stimulus/submit_jtag_tests.sh
+++ src/integration/stimulus/submit_jtag_tests.sh
@@ -22,3 +22,4 @@ submit --interactive --name css_regress --project Caliptra ss_build -tc smoke_te
 submit --interactive --name css_regress --project Caliptra ss_build -tc smoke_test_jtag_prod_dbg          -op -sb -to 10000000
 submit --interactive --name css_regress --project Caliptra ss_build -tc caliptra_ss_jtag_lcc_st_trans     -op -sb -to 85000
 submit --interactive --name css_regress --project Caliptra ss_build -tc smoke_test_jtag_lcc_registers     -op -sb -to 85000
+submit --interactive --name css_regress --project Caliptra ss_build -tc caliptra_ss_jtag_lcc_st_trans     -op -sb -to 220000
