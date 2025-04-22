28ea61a3b3f4223b122b81ff52826de6fbb5ae90
andrea-caforio <v-acaforio@microsoft.com>
2025-04-22 17:52:48 +0200

    [lc_ctrl, test] caliptra_ss_lcc_clock_bypass (#293)
    * [lc_ctrl, test] caliptra_ss_lcc_clock_bypass
    
    Signed-off-by: Andrea Caforio <andrea.caforio@lowrisc.org>
    
    * MICROSOFT AUTOMATED PIPELINE: Stamp 'caliptra_ss_lcc_clock_bypass' with updated timestamp and hash after successful run
    
    ---------
    
    Signed-off-by: Andrea Caforio <andrea.caforio@lowrisc.org>

---
+++ src/integration/test_suites/caliptra_ss_lcc_clock_bypass/caliptra_ss_lcc_clock_bypass.c
@@ -0,0 +1,73 @@
+#include <string.h>
+#include <stdint.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include "soc_address_map.h"
+#include "printf.h"
+#include "riscv_hw_if.h"
+#include "soc_ifc.h"
+#include "caliptra_ss_lc_ctrl_address_map.h"
+#include "caliptra_ss_lib.h"
+#include "fuse_ctrl.h"
+#include "lc_ctrl.h"
+
+volatile char* stdout = (char *)SOC_MCI_TOP_MCI_REG_DEBUG_OUT;
+#ifdef CPT_VERBOSITY
+    enum printf_verbosity verbosity_g = CPT_VERBOSITY;
+#else
+    enum printf_verbosity verbosity_g = LOW;
+#endif
+
+void clock_bypass() {
+    const uint32_t claim_trans_val = 0x96;
+
+    const uint32_t freqs[4] = {
+        CMD_FC_LCC_EXT_CLK_500MHZ,
+        CMD_FC_LCC_EXT_CLK_160MHZ,
+        CMD_FC_LCC_EXT_CLK_400MHZ,
+        CMD_FC_LCC_EXT_CLK_1000MHZ
+    };
+
+    wait_dai_op_idle(0);
+    lsu_write_32(SOC_MCI_TOP_MCI_REG_DEBUG_OUT, freqs[xorshift32() % 4]);
+
+    uint32_t reg_value, loop_ctrl;
+    while (loop_ctrl != claim_trans_val) {
+        lsu_write_32(LC_CTRL_CLAIM_TRANSITION_IF_OFFSET, claim_trans_val);
+        reg_value = lsu_read_32(LC_CTRL_CLAIM_TRANSITION_IF_OFFSET);
+        loop_ctrl = reg_value & claim_trans_val;
+    }
+
+    // Set TRANSITION_CTRL.VOLATILE_RAW_UNLOCK
+    lsu_write_32(LC_CTRL_TRANSITION_CTRL_OFFSET, 0x1);
+    wait_dai_op_idle(0);
+
+    initialize_otp_controller();
+
+    // Perform a state transition to see whether it still works with
+    // the external clock.
+    transition_state(TEST_LOCKED0, 0, 0, 0, 0, 0);
+    wait_dai_op_idle(0);
+}
+
+void main (void) {
+    VPRINTF(LOW, "=================\nMCU Caliptra Boot Go\n=================\n\n")
+    
+    mcu_cptra_init_d();
+    wait_dai_op_idle(0);
+      
+    lcc_initialization();
+    grant_mcu_for_fc_writes(); 
+
+    transition_state(TEST_UNLOCKED0, raw_unlock_token[0], raw_unlock_token[1], raw_unlock_token[2], raw_unlock_token[3], 1);
+    wait_dai_op_idle(0);
+
+    clock_bypass();
+
+    for (uint8_t ii = 0; ii < 160; ii++) {
+        __asm__ volatile ("nop"); // Sleep loop as "nop"
+    }
+
+    SEND_STDOUT_CTRL(0xff);
+}
