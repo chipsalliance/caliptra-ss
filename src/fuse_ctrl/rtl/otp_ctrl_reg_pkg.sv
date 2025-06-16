fc419059a596b58d4ab0166262d5d9c2194919a0
Andrea Caforio <andrea.caforio@lowrisc.org>
2025-06-16 07:01:19 -0700

    [fuse_ctrl, rtl] Zeroization DAI command
    A new zeroization command to the direct access interface (DAI) is
    added:
    
     - DaiZeroize: Zeroize fuse and return zeroized value.
    
    This commit serves as the starting point for the actual
    implementation of this command.
    
    Signed-off-by: Andrea Caforio <andrea.caforio@lowrisc.org>

--- src/fuse_ctrl/rtl/otp_ctrl_reg_pkg.sv
+++ src/fuse_ctrl/rtl/otp_ctrl_reg_pkg.sv
@@ -474,6 +474,10 @@ package otp_ctrl_reg_pkg;
     struct packed {
       logic        q;
       logic        qe;
+    } zeroize;
+    struct packed {
+      logic        q;
+      logic        qe;
     } digest;
     struct packed {
       logic        q;
@@ -704,12 +708,12 @@ package otp_ctrl_reg_pkg;
 
   // Register -> HW type for core interface
   typedef struct packed {
-    otp_ctrl_reg2hw_intr_state_reg_t intr_state; // [240:239]
-    otp_ctrl_reg2hw_intr_enable_reg_t intr_enable; // [238:237]
-    otp_ctrl_reg2hw_intr_test_reg_t intr_test; // [236:233]
-    otp_ctrl_reg2hw_alert_test_reg_t alert_test; // [232:223]
-    otp_ctrl_reg2hw_direct_access_regwen_reg_t direct_access_regwen; // [222:221]
-    otp_ctrl_reg2hw_direct_access_cmd_reg_t direct_access_cmd; // [220:215]
+    otp_ctrl_reg2hw_intr_state_reg_t intr_state; // [242:241]
+    otp_ctrl_reg2hw_intr_enable_reg_t intr_enable; // [240:239]
+    otp_ctrl_reg2hw_intr_test_reg_t intr_test; // [238:235]
+    otp_ctrl_reg2hw_alert_test_reg_t alert_test; // [234:225]
+    otp_ctrl_reg2hw_direct_access_regwen_reg_t direct_access_regwen; // [224:223]
+    otp_ctrl_reg2hw_direct_access_cmd_reg_t direct_access_cmd; // [222:215]
     otp_ctrl_reg2hw_direct_access_address_reg_t direct_access_address; // [214:203]
     otp_ctrl_reg2hw_direct_access_wdata_mreg_t [1:0] direct_access_wdata; // [202:139]
     otp_ctrl_reg2hw_check_trigger_reg_t check_trigger; // [138:135]
@@ -914,10 +918,11 @@ package otp_ctrl_reg_pkg;
   parameter logic [2:0] OTP_CTRL_ERR_CODE_17_ERR_CODE_17_RESVAL = 3'h 0;
   parameter logic [0:0] OTP_CTRL_DIRECT_ACCESS_REGWEN_RESVAL = 1'h 1;
   parameter logic [0:0] OTP_CTRL_DIRECT_ACCESS_REGWEN_DIRECT_ACCESS_REGWEN_RESVAL = 1'h 1;
-  parameter logic [2:0] OTP_CTRL_DIRECT_ACCESS_CMD_RESVAL = 3'h 0;
+  parameter logic [3:0] OTP_CTRL_DIRECT_ACCESS_CMD_RESVAL = 4'h 0;
   parameter logic [0:0] OTP_CTRL_DIRECT_ACCESS_CMD_RD_RESVAL = 1'h 0;
   parameter logic [0:0] OTP_CTRL_DIRECT_ACCESS_CMD_WR_RESVAL = 1'h 0;
   parameter logic [0:0] OTP_CTRL_DIRECT_ACCESS_CMD_DIGEST_RESVAL = 1'h 0;
+  parameter logic [0:0] OTP_CTRL_DIRECT_ACCESS_CMD_ZEROIZE_RESVAL = 1'h 0;
   parameter logic [31:0] OTP_CTRL_DIRECT_ACCESS_RDATA_0_RESVAL = 32'h 0;
   parameter logic [31:0] OTP_CTRL_DIRECT_ACCESS_RDATA_0_DIRECT_ACCESS_RDATA_0_RESVAL = 32'h 0;
   parameter logic [31:0] OTP_CTRL_DIRECT_ACCESS_RDATA_1_RESVAL = 32'h 0;
