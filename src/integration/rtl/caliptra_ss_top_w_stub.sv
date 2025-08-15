42cddd3893b354b3b0f7c2026a7cee7267781814
Emre Karabulut <emre.karabulut.dde@gmail.com>
2025-08-15 13:38:57 -0700

    fixed abr lint connection error

--- src/integration/rtl/caliptra_ss_top_w_stub.sv
+++ src/integration/rtl/caliptra_ss_top_w_stub.sv
@@ -170,17 +170,17 @@ module caliptra_ss_top_w_stub(
     assign cptra_ss_cptra_core_el2_mem_export.iccm_bank_ecc = '0;
     assign cptra_ss_cptra_core_el2_mem_export.dccm_bank_dout = '0;
     assign cptra_ss_cptra_core_el2_mem_export.iccm_bank_dout = '0;
-    mldsa_mem_if mldsa_memory_export();
-    assign mldsa_memory_export.mem_inst0_bank0_rdata_o = '0;
-    assign mldsa_memory_export.mem_inst0_bank1_rdata_o = '0;
-    assign mldsa_memory_export.mem_inst1_rdata_o = '0;
-    assign mldsa_memory_export.mem_inst2_rdata_o = '0;
-    assign mldsa_memory_export.mem_inst3_rdata_o = '0;
-    assign mldsa_memory_export.sig_z_mem_rdata_o = '0;
-    assign mldsa_memory_export.pk_mem_rdata_o = '0;
-    assign mldsa_memory_export.sk_mem_bank0_rdata_o = '0;
-    assign mldsa_memory_export.sk_mem_bank1_rdata_o = '0;
-    assign mldsa_memory_export.w1_mem_rdata_o = '0;
+    abr_mem_if abr_memory_export();
+    assign abr_memory_export.mem_inst0_bank0_rdata_o = '0;
+    assign abr_memory_export.mem_inst0_bank1_rdata_o = '0;
+    assign abr_memory_export.mem_inst1_rdata_o = '0;
+    assign abr_memory_export.mem_inst2_rdata_o = '0;
+    assign abr_memory_export.mem_inst3_rdata_o = '0;
+    assign abr_memory_export.sig_z_mem_rdata_o = '0;
+    assign abr_memory_export.pk_mem_rdata_o = '0;
+    assign abr_memory_export.sk_mem_bank0_rdata_o = '0;
+    assign abr_memory_export.sk_mem_bank1_rdata_o = '0;
+    assign abr_memory_export.w1_mem_rdata_o = '0;
 
 
     logic cptra_ss_cptra_core_mbox_sram_cs_o;
@@ -472,7 +472,7 @@ module caliptra_ss_top_w_stub(
 
     // Caliptra Memory Export Interface
         .cptra_ss_cptra_core_el2_mem_export(cptra_ss_cptra_core_el2_mem_export),
-        .mldsa_memory_export_req(mldsa_memory_export.req),
+        .abr_memory_export_req(abr_memory_export.req),
     
     //SRAM interface for mbox
         .cptra_ss_cptra_core_mbox_sram_cs_o,
