0e9f0897f13dd471db46df236558e473b9183301
Emre Karabulut <emre.karabulut.dde@gmail.com>
2025-08-25 13:08:39 -0700

    fixed the comment

--- src/integration/rtl/caliptra_ss_top.sv
+++ src/integration/rtl/caliptra_ss_top.sv
@@ -263,7 +263,6 @@ module caliptra_ss_top
     input  logic cptra_i3c_axi_user_id_filtering_enable_i,
 
     input  logic [63:0] cptra_ss_cptra_core_generic_input_wires_i,
-    // -- caliptra DUT instance
     output logic [63:0] cptra_ss_cptra_core_generic_output_wires_o,
     input  logic        cptra_ss_cptra_core_scan_mode_i,
     output logic        cptra_error_fatal,
