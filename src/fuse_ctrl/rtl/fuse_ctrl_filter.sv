231578e5b83fa128d3c5c3ae7945be7116176d0f
Emre Karabulut <emre.karabulut.dde@gmail.com>
2025-09-05 16:18:18 -0700

    added new rules for UDS/FE zeroization

--- src/fuse_ctrl/rtl/fuse_ctrl_filter.sv
+++ src/fuse_ctrl/rtl/fuse_ctrl_filter.sv
@@ -27,6 +27,7 @@ module fuse_ctrl_filter
     input wire clk_i,
     input wire rst_n_i,
     input wire fc_init_done,
+    input wire FIPS_ZEROIZATION_CMD_i,
 
     input logic [31:0] cptra_ss_strap_mcu_lsu_axi_user_i,
     input logic [31:0] cptra_ss_strap_cptra_axi_user_i,
@@ -44,14 +45,17 @@ module fuse_ctrl_filter
 // Internal signal declarations
 //---------------------------------------------------------------------
 fc_table_state_t table_fsm_current_st, table_fsm_next_st;
-logic first_write_addr, second_write_addr, write_event, trigger_table_check, partition_cmd_axi_addr, all_same_id;
-
+logic first_write_addr, second_write_addr, first_read_addr, second_read_addr, write_event, trigger_table_check, partition_cmd_axi_addr, all_same_id, addr_and_cmd_same_id;
+logic caliptra_secret_access;
 logic [FC_TABLE_NUM_RANGES-1:0] wr_allowed_vec;
 logic wr_req_allowed;
 logic [31:0] latched_fuse_addr;
 logic [31:0]  req_axi_user_id, latched_data_id0, latched_data_id1, latched_addr_id, latched_cmd_id;
 logic latch_addr, latch_data_id0, latch_data_id1, latch_addr_id, latch_cmd_id, clear_records;
 logic discard_fuse_write;
+
+
+dai_cmd_e fuse_cmd;
 
 //---------------------------------------------------------------------
 // Extract AXI user ID from the core write request
@@ -69,29 +73,39 @@ always_ff @(posedge clk_i or negedge rst_n_i) begin
         latched_addr_id         <= '0;
         latched_cmd_id          <= '0;
         discard_fuse_write_o    <= '0;
+        fuse_cmd                <= DaiRead;
+        caliptra_secret_access  <= '0;
     end else begin
         table_fsm_current_st <= table_fsm_next_st;
         discard_fuse_write_o <= discard_fuse_write;
         // If clear_records is asserted, reset all latched registers
         if (clear_records) begin
-            latched_fuse_addr <= '0;
-            latched_data_id0  <= '0;
-            latched_data_id1  <= '0;
-            latched_addr_id   <= '0;
-            latched_cmd_id    <= '0;
+            latched_fuse_addr       <= '0;
+            latched_data_id0        <= '0;
+            latched_data_id1        <= '0;
+            latched_addr_id         <= '0;
+            latched_cmd_id          <= '0;
+            fuse_cmd                <= DaiRead;
+            caliptra_secret_access  <= '0;
         end else begin
             if (latch_addr)
                 latched_fuse_addr <= core_axi_wr_req.wdata;
-            if (latch_data_id0) begin
+
+            if (latch_data_id0) 
                 latched_data_id0 <= req_axi_user_id;
+
+            if (latch_data_id1)
                 latched_data_id1 <= req_axi_user_id;
-            end else if (latch_data_id1) begin
-                latched_data_id1 <= req_axi_user_id;
+            
+            if (latch_addr_id) begin
+                latched_addr_id         <= req_axi_user_id;
+                caliptra_secret_access  <= (core_axi_wr_req.wdata >= CALIPTRA_SECRET_ACCESS_LOWER_ADDR && core_axi_wr_req.wdata >= CALIPTRA_SECRET_ACCESS_UPPER_ADDR) ? 1'b1 : 1'b0;
             end
-            if (latch_addr_id)
-                latched_addr_id <= req_axi_user_id;
-            if (latch_cmd_id)
-                latched_cmd_id <= req_axi_user_id;
+
+            if (latch_cmd_id) begin
+                latched_cmd_id  <= req_axi_user_id;
+                fuse_cmd        <= dai_cmd_e'(core_axi_wr_req.wdata[31:0]);
+            end
         end
     end
 end
@@ -109,7 +123,7 @@ always_comb begin
     for (int i = 0; i < FC_TABLE_NUM_RANGES; i = i + 1) begin : gen_wr_allowed
         wr_allowed_vec[i] = ( (latched_fuse_addr >= access_control_table[i].lower_addr)
                              && (latched_fuse_addr <= access_control_table[i].upper_addr)
-                             && (latched_data_id0 == AXI_user_id_arr[i]));
+                             && (latched_cmd_id == AXI_user_id_arr[i]));
     end
 end
 
@@ -130,28 +144,48 @@ always_comb begin
             end
         end
         //-------------------------------------------------------------------------
-        // IDLE_ST: Monitor for a valid write address for either the first or second fuse data.
+        // IDLE_ST: Monitor for a valid write/read address for either the first or second fuse data.
         // If a valid address is detected, latch the corresponding data ID and transition to WDATA_ADDR_ST.
-        // Otherwise, clear records and remain in IDLE.
+        // Otherwise, remain in IDLE.
         IDLE_ST: begin            
-            latch_addr        = 1'b0;
-            latch_addr_id     = 1'b0;
-            latch_cmd_id      = 1'b0;
-            discard_fuse_write= 1'b0;
+            latch_addr          = 1'b0;
+            discard_fuse_write  = 1'b0;
+            clear_records       = 1'b0;
             if (first_write_addr) begin
                 latch_data_id0     = 1'b1;
                 latch_data_id1     = 1'b0;
-                clear_records      = 1'b0;
+                latch_addr_id      = 1'b0;
+                latch_cmd_id       = 1'b0;
                 table_fsm_next_st  = WDATA_ADDR_ST;
             end else if (second_write_addr) begin
                 latch_data_id0     = 1'b0;
                 latch_data_id1     = 1'b1;
-                clear_records      = 1'b0;
+                latch_addr_id      = 1'b0;
+                latch_cmd_id       = 1'b0;
                 table_fsm_next_st  = WDATA_ADDR_ST;
+            end else if (trigger_table_check) begin
+                latch_data_id0     = 1'b0;
+                latch_data_id1     = 1'b0;
+                latch_addr_id      = 1'b1;
+                latch_cmd_id       = 1'b0;
+                table_fsm_next_st  = FUSE_ADDR_AXI_ADDR_ST;
+            end else if (first_write_addr || second_read_addr) begin
+                latch_data_id0     = 1'b0;
+                latch_data_id1     = 1'b0;
+                latch_addr_id      = 1'b0;
+                latch_cmd_id       = 1'b0;
+                table_fsm_next_st  = RDATA_ADDR_ST;
+            end else if (partition_cmd_axi_addr) begin
+                latch_data_id0     = 1'b0;
+                latch_data_id1     = 1'b0;
+                latch_addr_id      = 1'b0;
+                latch_cmd_id       = 1'b1;
+                table_fsm_next_st  = FUSE_CMD_AXI_ADDR_ST;
             end else begin
                 latch_data_id0     = 1'b0;
                 latch_data_id1     = 1'b0;
-                clear_records      = 1'b1;
+                latch_addr_id      = 1'b0;
+                latch_cmd_id       = 1'b0;
                 table_fsm_next_st  = IDLE_ST;
             end
         end     
@@ -171,36 +205,114 @@ always_comb begin
             end else begin
                 table_fsm_next_st = WDATA_ADDR_ST;
             end
-        end  
+        end   
+        //-------------------------------------------------------------------------
+        // RDATA_ADDR_ST: Wait for a valid write event that carries the data.
+        // Remain in this state until the write event is detected, then move to RDATA_ST.
+        RDATA_ADDR_ST: begin
+            latch_addr        = 1'b0;
+            latch_data_id0    = 1'b0;
+            latch_data_id1    = 1'b0;
+            latch_addr_id     = 1'b0;
+            latch_cmd_id      = 1'b0;
+            clear_records     = 1'b0;
+            discard_fuse_write= 1'b0;
+            if (write_event) begin
+                table_fsm_next_st = RDATA_ST;
+            end else begin
+                table_fsm_next_st = RDATA_ADDR_ST;
+            end
+        end 
         //-------------------------------------------------------------------------
         // WDATA_ST: Process the write data.
         // If a table check is triggered, latch the address ID and transition to the fuse address phase.
         // Otherwise, if additional data writes occur due to the granulartiy, latch the corresponding data ID.
         WDATA_ST: begin
             latch_addr        = 1'b0;
-            latch_cmd_id      = 1'b0;
             clear_records     = 1'b0;
             discard_fuse_write= 1'b0;
             if (trigger_table_check) begin
-                table_fsm_next_st = FUSE_ADDR_AXI_ADDR_ST;
-                latch_addr_id     = 1'b1;
-                latch_data_id0    = 1'b0;
-                latch_data_id1    = 1'b0;
+                latch_data_id0      = 1'b0;
+                latch_data_id1      = 1'b0;
+                latch_addr_id       = 1'b1;
+                latch_cmd_id        = 1'b0;
+                table_fsm_next_st   = FUSE_ADDR_AXI_ADDR_ST;
             end else if (first_write_addr) begin
-                table_fsm_next_st = WDATA_ADDR_ST;
-                latch_addr_id     = 1'b0;
-                latch_data_id0    = 1'b1;
-                latch_data_id1    = 1'b0;
+                latch_data_id0      = 1'b1;
+                latch_data_id1      = 1'b0;
+                latch_addr_id       = 1'b0;
+                latch_cmd_id        = 1'b0;
+                table_fsm_next_st   = WDATA_ADDR_ST;
             end else if (second_write_addr) begin
-                table_fsm_next_st = WDATA_ADDR_ST;
-                latch_addr_id     = 1'b0;
-                latch_data_id0    = 1'b0;
-                latch_data_id1    = 1'b1;
+                latch_data_id0      = 1'b0;
+                latch_data_id1      = 1'b1;
+                latch_addr_id       = 1'b0;
+                latch_cmd_id        = 1'b0;
+                table_fsm_next_st   = WDATA_ADDR_ST;
+            end else if (first_write_addr || second_read_addr) begin
+                latch_data_id0     = 1'b0;
+                latch_data_id1     = 1'b0;
+                latch_addr_id      = 1'b0;
+                latch_cmd_id       = 1'b0;
+                table_fsm_next_st  = RDATA_ADDR_ST;
+            end else if (partition_cmd_axi_addr) begin
+                latch_data_id0     = 1'b0;
+                latch_data_id1     = 1'b0;
+                latch_addr_id      = 1'b0;
+                latch_cmd_id       = 1'b1;
+                table_fsm_next_st  = FUSE_CMD_AXI_ADDR_ST;
             end else begin
-                table_fsm_next_st = WDATA_ST;
-                latch_addr_id     = 1'b0;
                 latch_data_id0    = 1'b0;
                 latch_data_id1    = 1'b0;
+                latch_addr_id     = 1'b0;
+                latch_cmd_id      = 1'b0;
+                table_fsm_next_st = WDATA_ST;
+            end
+        end
+        //-------------------------------------------------------------------------
+        // WDATA_ST: Process the write data.
+        // If a table check is triggered, latch the address ID and transition to the fuse address phase.
+        // Otherwise, if additional data writes occur due to the granulartiy, latch the corresponding data ID.
+        RDATA_ST: begin
+            latch_addr        = 1'b0;
+            clear_records     = 1'b0;
+            discard_fuse_write= 1'b0;
+            if (trigger_table_check) begin
+                latch_data_id0      = 1'b0;
+                latch_data_id1      = 1'b0;
+                latch_addr_id       = 1'b1;
+                latch_cmd_id        = 1'b0;
+                table_fsm_next_st   = FUSE_ADDR_AXI_ADDR_ST;
+            end else if (first_write_addr) begin
+                latch_data_id0      = 1'b1;
+                latch_data_id1      = 1'b0;
+                latch_addr_id       = 1'b0;
+                latch_cmd_id        = 1'b0;
+                table_fsm_next_st   = WDATA_ADDR_ST;
+            end else if (second_write_addr) begin
+                latch_data_id0      = 1'b0;
+                latch_data_id1      = 1'b1;
+                latch_addr_id       = 1'b0;
+                latch_cmd_id        = 1'b0;
+                table_fsm_next_st   = WDATA_ADDR_ST;
+            end else if (first_write_addr || second_read_addr) begin
+                latch_data_id0     = 1'b0;
+                latch_data_id1     = 1'b0;
+                latch_addr_id      = 1'b0;
+                latch_cmd_id       = 1'b0;
+                table_fsm_next_st  = RDATA_ADDR_ST;
+            end else if (partition_cmd_axi_addr) begin
+                latch_data_id0     = 1'b0;
+                latch_data_id1     = 1'b0;
+                latch_addr_id      = 1'b0;
+                latch_cmd_id       = 1'b1;
+                table_fsm_next_st  = FUSE_CMD_AXI_ADDR_ST;
+            end else begin
+                latch_data_id0    = 1'b0;
+                latch_data_id1    = 1'b0;
+                latch_addr_id     = 1'b0;
+                latch_cmd_id      = 1'b0;
+                table_fsm_next_st = RDATA_ST;
             end
         end
         //-------------------------------------------------------------------------
@@ -226,17 +338,44 @@ always_comb begin
         // If the partition command address is detected, latch the command ID and move to FUSE_CMD_AXI_ADDR_ST.       
         FUSE_ADDR_AXI_WR_ST: begin
             latch_addr        = 1'b0;
-            latch_data_id0    = 1'b0;
-            latch_data_id1    = 1'b0;
-            latch_addr_id     = 1'b0;
             clear_records     = 1'b0;
             discard_fuse_write= 1'b0;
-            if (partition_cmd_axi_addr) begin
-                table_fsm_next_st = FUSE_CMD_AXI_ADDR_ST;
-                latch_cmd_id      = 1'b1;
+            if (trigger_table_check) begin
+                latch_data_id0      = 1'b0;
+                latch_data_id1      = 1'b0;
+                latch_addr_id       = 1'b1;
+                latch_cmd_id        = 1'b0;
+                table_fsm_next_st   = FUSE_ADDR_AXI_ADDR_ST;
+            end else if (first_write_addr) begin
+                latch_data_id0      = 1'b1;
+                latch_data_id1      = 1'b0;
+                latch_addr_id       = 1'b0;
+                latch_cmd_id        = 1'b0;
+                table_fsm_next_st   = WDATA_ADDR_ST;
+            end else if (second_write_addr) begin
+                latch_data_id0      = 1'b0;
+                latch_data_id1      = 1'b1;
+                latch_addr_id       = 1'b0;
+                latch_cmd_id        = 1'b0;
+                table_fsm_next_st   = WDATA_ADDR_ST;
+            end else if (first_write_addr || second_read_addr) begin
+                latch_data_id0     = 1'b0;
+                latch_data_id1     = 1'b0;
+                latch_addr_id      = 1'b0;
+                latch_cmd_id       = 1'b0;
+                table_fsm_next_st  = RDATA_ADDR_ST;
+            end else if (partition_cmd_axi_addr) begin
+                latch_data_id0     = 1'b0;
+                latch_data_id1     = 1'b0;
+                latch_addr_id      = 1'b0;
+                latch_cmd_id       = 1'b1;
+                table_fsm_next_st  = FUSE_CMD_AXI_ADDR_ST;
             end else begin
-                table_fsm_next_st = FUSE_ADDR_AXI_WR_ST;
+                latch_data_id0    = 1'b0;
+                latch_data_id1    = 1'b0;
+                latch_addr_id     = 1'b0;
                 latch_cmd_id      = 1'b0;
+                table_fsm_next_st = RDATA_ST;
             end
         end
         //-------------------------------------------------------------------------
@@ -250,12 +389,42 @@ always_comb begin
             latch_addr_id     = 1'b0;
             latch_cmd_id      = 1'b0;
             clear_records     = 1'b0;
-            if (! wr_req_allowed || !all_same_id)begin
+            if (fuse_cmd == DaiRead && write_event)begin
+                discard_fuse_write= 1'b0;
+                table_fsm_next_st = IDLE_ST;
+
+            end else if (fuse_cmd == DaiWrite && write_event && !wr_req_allowed)begin
                 discard_fuse_write= 1'b1;
                 table_fsm_next_st = DISCARD_FUSE_CMD_AXI_WR_ST;
+
+            end else if (fuse_cmd == DaiWrite && write_event && !all_same_id)begin
+                discard_fuse_write= 1'b1;
+                table_fsm_next_st = DISCARD_FUSE_CMD_AXI_WR_ST;
+
+            end else if (fuse_cmd == DaiDigest && write_event && (caliptra_secret_access && !FIPS_ZEROIZATION_CMD_i))begin
+                discard_fuse_write= 1'b1;
+                table_fsm_next_st = DISCARD_FUSE_CMD_AXI_WR_ST;
+
+            end else if (fuse_cmd == DaiDigest && write_event && !wr_req_allowed)begin
+                discard_fuse_write= 1'b1;
+                table_fsm_next_st = DISCARD_FUSE_CMD_AXI_WR_ST;
+
+            end else if (fuse_cmd == DaiDigest && write_event && !addr_and_cmd_same_id)begin
+                discard_fuse_write= 1'b1;
+                table_fsm_next_st = DISCARD_FUSE_CMD_AXI_WR_ST;
+
+            end else if (fuse_cmd == DaiZeroize && write_event && !wr_req_allowed)begin
+                discard_fuse_write= 1'b1;
+                table_fsm_next_st = DISCARD_FUSE_CMD_AXI_WR_ST;
+
+            end else if (fuse_cmd == DaiZeroize && write_event && !addr_and_cmd_same_id)begin
+                discard_fuse_write= 1'b1;
+                table_fsm_next_st = DISCARD_FUSE_CMD_AXI_WR_ST;
+
             end else if (write_event) begin
                 discard_fuse_write= 1'b0;
                 table_fsm_next_st = IDLE_ST;
+
             end else begin
                 discard_fuse_write= 1'b0;
                 table_fsm_next_st = FUSE_CMD_AXI_ADDR_ST;
@@ -303,10 +472,16 @@ end
 always_comb begin
     first_write_addr            = (core_axi_wr_req.awvalid && core_axi_wr_rsp.awready && core_axi_wr_req.awaddr[CoreAw-1:0] == OTP_CTRL_DIRECT_ACCESS_WDATA_0_OFFSET);
     second_write_addr           = (core_axi_wr_req.awvalid && core_axi_wr_rsp.awready && core_axi_wr_req.awaddr[CoreAw-1:0] == OTP_CTRL_DIRECT_ACCESS_WDATA_1_OFFSET);
+
+    first_read_addr            = (core_axi_wr_req.awvalid && core_axi_wr_rsp.awready && core_axi_wr_req.awaddr[CoreAw-1:0] == OTP_CTRL_DIRECT_ACCESS_RDATA_0_OFFSET);
+    second_read_addr           = (core_axi_wr_req.awvalid && core_axi_wr_rsp.awready && core_axi_wr_req.awaddr[CoreAw-1:0] == OTP_CTRL_DIRECT_ACCESS_RDATA_1_OFFSET);
+
     write_event                 = (core_axi_wr_req.wvalid && core_axi_wr_rsp.wready);
     trigger_table_check         = (core_axi_wr_req.awvalid && core_axi_wr_rsp.awready && core_axi_wr_req.awaddr[CoreAw-1:0] == OTP_CTRL_DIRECT_ACCESS_ADDRESS_OFFSET);
     partition_cmd_axi_addr      = (core_axi_wr_req.awvalid && core_axi_wr_rsp.awready && core_axi_wr_req.awaddr[CoreAw-1:0] == OTP_CTRL_DIRECT_ACCESS_CMD_OFFSET);
     all_same_id                 = (latched_data_id1 == latched_data_id0) && (latched_data_id0 == latched_addr_id) &&  (latched_addr_id == latched_cmd_id);
+
+    addr_and_cmd_same_id        = (latched_addr_id == latched_cmd_id);
 end
 
 
