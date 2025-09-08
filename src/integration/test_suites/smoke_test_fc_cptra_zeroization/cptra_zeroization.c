0e41b06bf80c3eb797a48e1db7ac949fe516c261
EMRE KARABULUT <63821295+ekarabu@users.noreply.github.com>
2025-09-07 19:24:08 -0500

    User/ekarabulut/uds fe 0ing test (#702)
    * update fuse map to zeroized UDS and FE
    
    * added new rules for UDS/FE zeroization
    
    * fixed /2 bug in tpl files
    
    * added more fc filter tests
    
    * fuse filter smoke test pass
    
    * update fuse map to zeroized UDS and FE
    
    * [tools] Add image overlay support to `gen_fuse_ctrl_vmem`
    
    This change adds support for fuse overlay configuration files in hjson
    format. Overlay files can be used to customize the default fuse
    configuration for a given test.
    
    For example, given the following overlay:
    
    ```json
    {
        partitions: [
            {
                name: "SECRET_MANUF_PARTITION",
                lock: "True",
                items: [
                    {
                        name: "CPTRA_CORE_UDS_SEED",
                        value: "<random>"
                    }
                ]
            },
    }
    ```
    
    The tool generates the following output:
    
    ```shell
    INFO: Overriding values of SECRET_MANUF_PARTITION partition.
    INFO: > Adding item CPTRA_CORE_UDS_SEED with size 64B and value:
    INFO:   000000: 69 75 98 08 d5 4c 9f 0d
    INFO:   000008: 58 f0 6f 5e 99 3c 0d 58
    INFO:   000010: 7d fb 50 b5 12 7c c9 c2
    INFO:   000018: ad be 46 4e e4 46 53 97
    INFO:   000020: e8 c0 c0 25 3a a9 6c 6f
    INFO:   000028: 4a 93 d8 f8 92 77 98 51
    INFO:   000030: eb 05 cf e3 d2 37 61 fa
    INFO:   000038: 90 1f 14 34 52 6c aa e8
    
    [...]
    
    INFO: Streamout of partition SECRET_MANUF_PARTITION
    INFO: > Scramble partition with key "SecretManufKey"
    INFO: > Lock partition by computing digest
    ```
    
    See `gen_fuse_ctrl_vmem.py` for more details.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
    (cherry picked from commit 5d0a5deab4e671d58c260dedbe93b37c448c70d6)
    
    * [gitignore] Ignore Python compiled binaries.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
    (cherry picked from commit 36a2187b482959dc40aac8e15be006bfb3704cb2)
    
    * [fuse_ctrl] Add UDS and FS zeroization tests.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
    (cherry picked from commit 1def9084f23a806f1adf88cb72d48c19190db352)
    
    * added new rules for UDS/FE zeroization
    
    * fixed /2 bug in tpl files
    
    * added more fc filter tests
    
    * fuse filter smoke test pass
    
    * [fuse_ctrl] Grant write access to MCU and Core in zeroization test.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
    
    * activ 2 write ID check only for UDS/FE
    
    * fixed broken rebase
    
    * fixed sva and test
    
    * [fc_ctrl] Add UDS/FE zeroization from Caliptra.
    
    Tests successful completion of UDS/FE zeroization from Caliptra core.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
    
    * [fuse_ctrl] Simplify MCU UDS/FE test.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
    
    * added mcu_cptra_init
    
    * added debug intent to enable prod reg use
    
    * MICROSOFT AUTOMATED PIPELINE: Stamp 'user/ekarabulut/UDS_FE_0ing_test' with updated timestamp and hash after successful run
    
    ---------
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
    Co-authored-by: Miguel Osorio <miguelosorio@google.com>

--- src/integration/test_suites/smoke_test_fc_cptra_zeroization/cptra_zeroization.c
+++ src/integration/test_suites/smoke_test_fc_cptra_zeroization/cptra_zeroization.c
@@ -28,7 +28,7 @@
 #include "soc_ifc.h"
 #include "veer-csr.h"
 
-volatile uint32_t* stdout = (uint32_t*)STDOUT;
+volatile uint32_t *stdout = (uint32_t *)STDOUT;
 volatile uint32_t intr_count = 0;
 #ifdef CPT_VERBOSITY
 enum printf_verbosity verbosity_g = CPT_VERBOSITY;
@@ -41,7 +41,7 @@ volatile caliptra_intr_received_s cptra_intr_rcv = {0};
 #define LOG_INFO(...) VPRINTF(LOW, "CLP_CORE:" #__VA_ARGS__)
 
 static inline void sleep(const uint32_t cycles) {
-  for (uint8_t ii = 0; ii < cycles; ii++) {
+  for (uint32_t ii = 0; ii < cycles; ii++) {
     __asm__ volatile("nop");
   }
 }
@@ -114,7 +114,7 @@ void dma_dai_wr(uint32_t addr, uint32_t wdata0, uint32_t wdata1,
   dma_wait_dai_op_idle(exp_status);
 }
 
-static void dma_dai_rd(uint32_t addr, uint32_t* rdata0, uint32_t* rdata1,
+static void dma_dai_rd(uint32_t addr, uint32_t *rdata0, uint32_t *rdata1,
                        uint32_t granularity, uint32_t exp_status) {
   VPRINTF(LOW, "CLP_CORE: Starting DAI read operation...\n");
 
@@ -139,7 +139,7 @@ static void dma_dai_rd(uint32_t addr, uint32_t* rdata0, uint32_t* rdata1,
   }
 }
 
-void dma_dai_zer(uint32_t addr, uint32_t* rdata0, uint32_t* rdata1,
+void dma_dai_zer(uint32_t addr, uint32_t *rdata0, uint32_t *rdata1,
                  uint32_t granularity, uint32_t exp_status) {
   VPRINTF(LOW, "CLP_CORE: Starting DAI zeroization operation...\n");
 
@@ -164,17 +164,15 @@ void dma_dai_zer(uint32_t addr, uint32_t* rdata0, uint32_t* rdata1,
   }
 }
 
-static uint32_t zeroize_partition(uint32_t partition_id) {
-  enum {
-    kPass = SOC_IFC_REG_SS_DBG_SERVICE_REG_RSP_UDS_PROGRAM_SUCCESS_MASK,
-    kFail = SOC_IFC_REG_SS_DBG_SERVICE_REG_RSP_UDS_PROGRAM_FAIL_MASK,
-  };
+static bool zeroize_partition(uint32_t partition_id) {
+
+  VPRINTF(LOW, "CLP_CORE: Zeroizing partition_id: %d\n", partition_id);
 
   if (partition_id >= NUM_PARTITIONS) {
-    return kFail;
+    return false;
   }
 
-  partition_t* p = &partitions[partition_id];
+  partition_t *p = &partitions[partition_id];
   const uint32_t kGranularity = p->granularity / CHAR_BIT;
   uint32_t read_data[2];
 
@@ -183,7 +181,7 @@ static uint32_t zeroize_partition(uint32_t partition_id) {
   if (p->zer_address == 0) {
     VPRINTF(LOW, "ERROR: partition_id: %d has no zeroize marker\n",
             partition_id);
-    return kFail;
+    return false;
   }
 
   // Read the digest to determine if the partition is locked. Read via DAI to
@@ -191,7 +189,7 @@ static uint32_t zeroize_partition(uint32_t partition_id) {
   dma_dai_rd(p->digest_address, &read_data[0], &read_data[1], 64, 0);
   if (read_data[0] == 0 && read_data[1] == 0) {
     VPRINTF(LOW, "ERROR: partition_id: %d is NOT locked\n", partition_id);
-    return kFail;
+    return false;
   }
 
   // Zeroize marker field.
@@ -199,7 +197,7 @@ static uint32_t zeroize_partition(uint32_t partition_id) {
   if (read_data[0] != 0xFFFFFFFF || read_data[1] != 0xFFFFFFFF) {
     VPRINTF(LOW, "ERROR: partition_id: %d marker is not zeroized\n",
             partition_id);
-    return kFail;
+    return false;
   }
 
   // Zeroize fuse fields.
@@ -211,7 +209,7 @@ static uint32_t zeroize_partition(uint32_t partition_id) {
       VPRINTF(LOW,
               "ERROR: fuse at addr 0x%08X is not zeroized, got: 0x%08X%08X\n",
               addr, read_data[1], read_data[0]);
-      return kFail;
+      return false;
     }
   }
 
@@ -220,52 +218,41 @@ static uint32_t zeroize_partition(uint32_t partition_id) {
   if (read_data[0] != 0xFFFFFFFF || read_data[1] != 0xFFFFFFFF) {
     VPRINTF(LOW, "ERROR: partition_id: %d digest is not zeroized\n",
             partition_id);
-    return kFail;
+    return false;
   }
 
-  return kPass;
+  return true;
 }
 
 void main(void) {
-  VPRINTF(LOW, "Caliptra: Mimicking ROM from Subsystem!!\n");
+  VPRINTF(LOW, "=====================================\n"
+               "Caliptra: Mimicking ROM from Subsystem!!\n"
+               "=====================================\n\n");
+
+  const uint32_t kPartitionIds[] = {
+      SECRET_MANUF_PARTITION,  SECRET_PROD_PARTITION_0, SECRET_PROD_PARTITION_1,
+      SECRET_PROD_PARTITION_2, SECRET_PROD_PARTITION_3,
+  };
+  const uint32_t kNumPartitions =
+      sizeof(kPartitionIds) / sizeof(kPartitionIds[0]);
 
   // Service zeroization requests.
-  while (1) {
-    uint32_t status_reg = lsu_read_32(CLP_SOC_IFC_REG_SS_DBG_SERVICE_REG_REQ);
-    if (status_reg == SOC_IFC_REG_SS_DBG_SERVICE_REG_REQ_UDS_PROGRAM_REQ_MASK) {
-      VPRINTF(LOW, "CLP_CORE: detected zeroization request\n");
-
-      // Clear the request.
-      status_reg &= (SOC_IFC_REG_SS_DBG_SERVICE_REG_REQ_UDS_PROGRAM_REQ_MASK ^
-                     0xFFFFFFFF);
-      lsu_write_32(CLP_SOC_IFC_REG_SS_DBG_SERVICE_REG_REQ, status_reg);
-
-      status_reg =
-          lsu_read_32(CLP_SOC_IFC_REG_SS_DBG_SERVICE_REG_RSP) |
-          SOC_IFC_REG_SS_DBG_SERVICE_REG_RSP_UDS_PROGRAM_IN_PROGRESS_MASK;
-      VPRINTF(LOW, "CLP_CORE: Zeroization IN_PROGRESS\n");
-      lsu_write_32(CLP_SOC_IFC_REG_SS_DBG_SERVICE_REG_RSP, status_reg);
-
-      // Get the partition ID from the UDS Low Address Register.
-      uint32_t partition_id =
-          lsu_read_32(CLP_SOC_IFC_REG_SS_UDS_SEED_BASE_ADDR_L);
-
-      uint32_t result = zeroize_partition(partition_id);
-
-      // Set the result of the operation, while clearing any previous status not
-      // cleared by the caller.
-      status_reg = lsu_read_32(CLP_SOC_IFC_REG_SS_DBG_SERVICE_REG_RSP);
-      status_reg &=
-          ((SOC_IFC_REG_SS_DBG_SERVICE_REG_RSP_UDS_PROGRAM_IN_PROGRESS_MASK |
-            SOC_IFC_REG_SS_DBG_SERVICE_REG_RSP_UDS_PROGRAM_SUCCESS_MASK |
-            SOC_IFC_REG_SS_DBG_SERVICE_REG_RSP_UDS_PROGRAM_FAIL_MASK) ^
-           0xFFFFFFFF);
-      status_reg |= result;
-
-      VPRINTF(LOW, "CLP_CORE: Zeroization complete. Result: 0x%08X\n",
-              status_reg);
-      lsu_write_32(CLP_SOC_IFC_REG_SS_DBG_SERVICE_REG_RSP, status_reg);
+  for (uint32_t i = 0; i < kNumPartitions; i++) {
+    uint32_t partition_id = kPartitionIds[i];
+    if (!zeroize_partition(partition_id)) {
+      VPRINTF(LOW, "ERROR: zeroize_partition failed for partition_id: %d\n",
+              partition_id);
+      break;
     }
-    sleep(100);
   }
+
+  // Flag completion.
+  uint32_t reg =
+      lsu_read_32(CLP_SOC_IFC_REG_SS_DBG_SERVICE_REG_RSP) |
+      SOC_IFC_REG_SS_DBG_SERVICE_REG_RSP_PROD_DBG_UNLOCK_SUCCESS_MASK;
+  lsu_write_32(CLP_SOC_IFC_REG_SS_DBG_SERVICE_REG_RSP, reg);
+  VPRINTF(LOW, "\n\nCLP_CORE: set PROD_DBG_UNLOCK_SUCCESS high 0x%X...\n\n",
+          reg);
+
+  sleep(500000);
 }
---
+++ src/integration/test_suites/smoke_test_fc_mcu_uds_fe_zeroization/smoke_test_fc_mcu_uds_fe_zeroization.c
@@ -0,0 +1,197 @@
+//********************************************************************************
+// SPDX-License-Identifier: Apache-2.0
+//
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//********************************************************************************
+#include <stdint.h>
+
+#include "caliptra_ss_lc_ctrl_address_map.h"
+#include "caliptra_ss_lib.h"
+#include "fuse_ctrl.h"
+#include "fuse_ctrl_mmap.h"
+#include "lc_ctrl.h"
+#include "printf.h"
+#include "riscv_hw_if.h"
+#include "soc_address_map.h"
+#include "soc_ifc.h"
+
+volatile char *stdout = (char *)SOC_MCI_TOP_MCI_REG_DEBUG_OUT;
+#ifdef CPT_VERBOSITY
+enum printf_verbosity verbosity_g = CPT_VERBOSITY;
+#else
+enum printf_verbosity verbosity_g = LOW;
+#endif
+
+#define LOG_ERROR(...) VPRINTF(LOW, "MCU ERROR:" #__VA_ARGS__)
+#define LOG_INFO(...) VPRINTF(LOW, "MCU:" #__VA_ARGS__)
+
+// This struct is used to store the partition information used in the test.
+typedef struct partition_info {
+  // Partition identifier. Used to access partition in the partitions array.
+  uint32_t id;
+  // Offset to the digest0 register.
+  uint32_t digest0;
+  // Offset to the digest1 register.
+  uint32_t digest1;
+} partition_info_t;
+
+const partition_info_t kPartitionsInfo[] = {
+    {
+        .id = SECRET_MANUF_PARTITION,
+        .digest0 = SOC_OTP_CTRL_SECRET_MANUF_PARTITION_DIGEST_DIGEST_0,
+        .digest1 = SOC_OTP_CTRL_SECRET_MANUF_PARTITION_DIGEST_DIGEST_1,
+    },
+    {
+        .id = SECRET_PROD_PARTITION_0,
+        .digest0 = SOC_OTP_CTRL_SECRET_PROD_PARTITION_0_DIGEST_DIGEST_0,
+        .digest1 = SOC_OTP_CTRL_SECRET_PROD_PARTITION_0_DIGEST_DIGEST_1,
+    },
+    {
+        .id = SECRET_PROD_PARTITION_1,
+        .digest0 = SOC_OTP_CTRL_SECRET_PROD_PARTITION_1_DIGEST_DIGEST_0,
+        .digest1 = SOC_OTP_CTRL_SECRET_PROD_PARTITION_1_DIGEST_DIGEST_1,
+    },
+    {
+        .id = SECRET_PROD_PARTITION_2,
+        .digest0 = SOC_OTP_CTRL_SECRET_PROD_PARTITION_2_DIGEST_DIGEST_0,
+
+        .digest1 = SOC_OTP_CTRL_SECRET_PROD_PARTITION_2_DIGEST_DIGEST_1,
+    },
+    {
+        .id = SECRET_PROD_PARTITION_3,
+        .digest0 = SOC_OTP_CTRL_SECRET_PROD_PARTITION_3_DIGEST_DIGEST_0,
+        .digest1 = SOC_OTP_CTRL_SECRET_PROD_PARTITION_3_DIGEST_DIGEST_1,
+    },
+};
+const uint32_t kNumPartitions =
+    sizeof(kPartitionsInfo) / sizeof(kPartitionsInfo[0]);
+
+static bool zeroization_check_unfeasible(uint32_t partition_id) {
+  partition_t *p = &partitions[partition_id];
+  uint32_t read_data[2];
+
+  if (p->zer_address == 0) {
+    LOG_ERROR("Partition %d is not zeroizable\n", partition_id);
+    return false;
+  }
+
+  // Read the digest to determine if the partition is locked
+  dai_rd(p->digest_address, &read_data[0], &read_data[1], 64, 0);
+  if (read_data[0] == 0 && read_data[1] == 0) {
+    LOG_ERROR("Partition %d is not locked\n", partition_id);
+    return false;
+  }
+
+  // Check that the partition has not been zeroized already.
+  dai_rd(p->zer_address, &read_data[0], &read_data[1], 64, 0);
+  if (read_data[0] == 0xFFFFFFFF || read_data[1] == 0xFFFFFFFF) {
+    LOG_ERROR("Partition %d has already been zeroized\n", partition_id);
+    return false;
+  }
+
+  // Attempt to zeroize. We check sure that the zeroize flag is still 0
+  // to confirm that zeroization is not feasible from MCU.
+  dai_zer(p->zer_address, &read_data[0], &read_data[1], 64,
+          OTP_CTRL_STATUS_DAI_ERROR_MASK);
+  if (read_data[0] != 0 || read_data[1] != 0) {
+    LOG_ERROR("Zeroize flag was set to 0x%x%x\n", read_data[1], read_data[0]);
+    return false;
+  }
+
+  reset_fc_lcc_rtl();
+  wait_dai_op_idle(0);
+
+  return true;
+}
+
+// Check that the digest is zeroized or not.
+static bool check_digest(uint32_t partition_id, bool expected_zeroized) {
+  uint32_t digest[2];
+  digest[0] = lsu_read_32(kPartitionsInfo[partition_id].digest0);
+  digest[1] = lsu_read_32(kPartitionsInfo[partition_id].digest1);
+  if (expected_zeroized) {
+    return digest[0] == UINT32_MAX && digest[1] == UINT32_MAX;
+  }
+  // If not zeroized, the digest should not be all ones.
+  return digest[0] != UINT32_MAX && digest[1] != UINT32_MAX;
+}
+
+static bool mcu_zeroization_test(void) {
+  for (uint32_t i = 0; i < kNumPartitions; i++) {
+    uint32_t partition_id = kPartitionsInfo[i].id;
+    if (!zeroization_check_unfeasible(partition_id)) {
+      LOG_ERROR(
+          "Unexpected zeroization success for partition_id: %d from MCU - PPD "
+          "not set\n",
+          partition_id);
+      return false;
+    }
+
+    lsu_write_32(SOC_MCI_TOP_MCI_REG_DEBUG_OUT, CMD_FC_FORCE_ZEROIZATION);
+    wait_dai_op_idle(0);
+
+    if (!zeroization_check_unfeasible(partition_id)) {
+      LOG_ERROR(
+          "Unexpected zeroization success for partition_id: %d from MCU - PPD "
+          "set\n",
+          partition_id);
+      return false;
+    }
+
+    lsu_write_32(SOC_MCI_TOP_MCI_REG_DEBUG_OUT, CMD_RELEASE_ZEROIZATION);
+    wait_dai_op_idle(0);
+  }
+  return true;
+}
+
+void main(void) {
+  mcu_cptra_init_d();
+
+  VPRINTF(LOW, "=====================================\n"
+               "MCU Caliptra Boot Go\n"
+               "=====================================\n\n");
+
+
+  wait_dai_op_idle(0);
+  initialize_otp_controller();
+
+  
+  wait_dai_op_idle(0);
+  initialize_otp_controller();
+
+  // Before releasing Caliptra core, test that we are unable to zeroize any of
+  // the partitions with or without PPD set.
+  if (!mcu_zeroization_test()) {
+    LOG_ERROR("MCU zeroization test failed\n");
+  }
+
+  // Reset.
+  reset_fc_lcc_rtl();
+  wait_dai_op_idle(0);
+
+  // At this point, the partitions should not be zeroized.
+  for (uint32_t i = 0; i < kNumPartitions; i++) {
+    uint32_t partition_id = kPartitionsInfo[i].id;
+    if (!check_digest(partition_id, /*expected_zeroized=*/false)) {
+      LOG_ERROR("Partition %d is not zeroized\n", partition_id);
+      break;
+    }
+  }
+
+  for (uint8_t ii = 0; ii < 160; ii++) {
+    // Sleep loop as "nop".
+    __asm__ volatile("nop");
+  }
+  SEND_STDOUT_CTRL(0xff);
+}
