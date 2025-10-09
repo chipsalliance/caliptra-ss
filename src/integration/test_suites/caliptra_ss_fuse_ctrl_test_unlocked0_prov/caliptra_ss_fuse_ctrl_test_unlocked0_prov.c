chipsalliance/caliptra-ss

commit 5cc9dbe83807d9b2e941cdb0cd5efc44eaa153c1

Collected lowRISC changes: bundle 1 (#786)
* [fuse_ctrl,dv]: Fix FcLockedPartitionWriteLock_A with scrambling

This assertion is checking that a partition will respond with an error
to any DAI write once the partition has a digest.

Unfortunately, the timing wasn't quite right: if scrambling is enabled
for the partition, the write mechanism in otp_ctrl_dai spends two
extra cycles computing the scrambled address and data before it tries
to write the value and spots that the partition is locked.

Modelling the exact timing carefully is a bit fiddly (because you
can't do ##[my_variable] in SVA) so this cheats slightly by weakening
the assertion. It thus says that if we expect an error it will come
out in between 2 and 4 cycles (even though we could, in theory decide
whether the value should be 2 or 4).

I've also tidied up the assertion a bit to make it easier to read: it
took me a while to decipher, but we may as well make it easier for the
next person.

* [test] Make VPRINTF a more convenient macro

Use the standard trick with "do { ... } while(0)" to make something
that behaves like a single statement (and expects a semicolon).

Now, you can do things like

  if (a) VPRINTF(LOW, "a\n"); else VPRINTF(LOW, "not a\n");

and the if/else will match up correctly.

This commit touches a lot of files because there were quite a few
places where the macro was used without a trailing semicolon.
Fortunately, sed can help:

    for x in $(git grep -l 'VPRINTF[^;]*)$');    \
      do sed -i 's/\(VPRINTF[^;]*)\)$/\1;/' $x;  \
    done

That's not quite enough though! In the other cases, the VPRINTF call
was split across multiple lines or had a comment between the ; and the
newline. There were only 18 of them though, so I've gone through them
manually and checked that everything is terminated properly.

* [lc_ctrl,dv] Tiny improvements in debug message grammar

* [fuse_ctrl,dv] Tidy up smoke_test_fc_mcu_uds_fe_zeroization's prints

src/integration/test_suites/caliptra_ss_fuse_ctrl_test_unlocked0_prov/caliptra_ss_fuse_ctrl_test_unlocked0_prov.c
@@ -97,7 +97,7 @@ void test_unlocked0_provision() {
 }
 
 void main (void) {
-    VPRINTF(LOW, "=================\nMCU Caliptra Boot Go\n=================\n\n")
+    VPRINTF(LOW, "=================\nMCU Caliptra Boot Go\n=================\n\n");
     
     mcu_cptra_init_d();
     wait_dai_op_idle(0);

commit e19011fdc891068670c31710ae9c9b9d8dbf90f0

[fuse_ctrl,dv] Fix build of caliptra_ss_fuse_ctrl_test_unlocked0_prov
This accidentally got broken in 5cc9dbe.

src/integration/test_suites/caliptra_ss_fuse_ctrl_test_unlocked0_prov/caliptra_ss_fuse_ctrl_test_unlocked0_prov.c
@@ -51,7 +51,7 @@ void test_unlocked0_provision() {
     uint32_t rnd_fuse_addresses[NUM_PARTITIONS-1];
 
     for (uint32_t i = 0; i < (NUM_PARTITIONS-1); i++) {
-        if (is_caliptra_secret_addr(part_sel[i].address)) {
+        if (is_caliptra_secret_addr(partitions[i].address)) {
             grant_caliptra_core_for_fc_writes();
         } else {
             grant_mcu_for_fc_writes(); 
@@ -77,7 +77,7 @@ void test_unlocked0_provision() {
     wait_dai_op_idle(0);
 
     for (uint32_t i = 0; i < (NUM_PARTITIONS-1); i++) {
-        if (is_caliptra_secret_addr(part_sel[i].address)) {
+        if (is_caliptra_secret_addr(partitions[i].address)) {
             grant_caliptra_core_for_fc_writes();
         } else {
             grant_mcu_for_fc_writes(); 