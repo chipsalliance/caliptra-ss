chipsalliance/caliptra-ss

commit 5cc9dbe83807d9b2e941cdb0cd5efc44eaa153c1

Collected lowRISC changes: bundle 1 (#786)
* [fuse_ctrl,dv]: Fix FcLockedPartitionWriteLock_A with scrambling

This assertion is checking that a partition will respond with an error
to any DAI write once the partition has a digest.

Unfortunately, the timing wasn't quite right: if scrambling is enabled
for the partition, the write mechanism in otp_ctrl_dai spends two
extra cycles computing the scrambled address and data before it tries
to write the value and spots that the partition is locked.

Modelling the exact timing carefully is a bit fiddly (because you
can't do ##[my_variable] in SVA) so this cheats slightly by weakening
the assertion. It thus says that if we expect an error it will come
out in between 2 and 4 cycles (even though we could, in theory decide
whether the value should be 2 or 4).

I've also tidied up the assertion a bit to make it easier to read: it
took me a while to decipher, but we may as well make it easier for the
next person.

* [test] Make VPRINTF a more convenient macro

Use the standard trick with "do { ... } while(0)" to make something
that behaves like a single statement (and expects a semicolon).

Now, you can do things like

  if (a) VPRINTF(LOW, "a\n"); else VPRINTF(LOW, "not a\n");

and the if/else will match up correctly.

This commit touches a lot of files because there were quite a few
places where the macro was used without a trailing semicolon.
Fortunately, sed can help:

    for x in $(git grep -l 'VPRINTF[^;]*)$');    \
      do sed -i 's/\(VPRINTF[^;]*)\)$/\1;/' $x;  \
    done

That's not quite enough though! In the other cases, the VPRINTF call
was split across multiple lines or had a comment between the ; and the
newline. There were only 18 of them though, so I've gone through them
manually and checked that everything is terminated properly.

* [lc_ctrl,dv] Tiny improvements in debug message grammar

* [fuse_ctrl,dv] Tidy up smoke_test_fc_mcu_uds_fe_zeroization's prints

.github/workflow_metadata/pr_hash
@@ -1,1 +1,1 @@
-e3d29c38ddb8872d8e652b543403d0dcda46b245369e4f0eed8f350a2f20cbdd6522dbd89d5eef677a7a8b88571fdbd3
+ea73c280cb5d506d0b24f027099a987882a32699e79f0c58742085023e362150c8cf736f31521967de0f08556a25473a

.github/workflow_metadata/pr_timestamp
@@ -1,1 +1,1 @@
-1759429134
+1759446717

src/integration/test_suites/caliptra_ss_fuse_ctrl_init_fail/caliptra_ss_fuse_ctrl_init_fail.c
@@ -97,7 +97,7 @@ void init_fail() {
 }
 
 void main (void) {
-    VPRINTF(LOW, "=================\nMCU Caliptra Boot Go\n=================\n\n")
+    VPRINTF(LOW, "=================\nMCU Caliptra Boot Go\n=================\n\n");
     
     mcu_cptra_init_d();
     wait_dai_op_idle(0);

src/integration/test_suites/caliptra_ss_fuse_ctrl_test_unlocked0_prov/caliptra_ss_fuse_ctrl_test_unlocked0_prov.c
@@ -97,7 +97,7 @@ void test_unlocked0_provision() {
 }
 
 void main (void) {
-    VPRINTF(LOW, "=================\nMCU Caliptra Boot Go\n=================\n\n")
+    VPRINTF(LOW, "=================\nMCU Caliptra Boot Go\n=================\n\n");
     
     mcu_cptra_init_d();
     wait_dai_op_idle(0);

src/integration/test_suites/smoke_test_fc_cptra_zeroization/smoke_test_fc_cptra_zeroization.c
@@ -33,8 +33,8 @@ enum printf_verbosity verbosity_g = CPT_VERBOSITY;
 enum printf_verbosity verbosity_g = LOW;
 #endif
 
-#define LOG_ERROR(...) VPRINTF(LOW, "MCU ERROR:" #__VA_ARGS__)
-#define LOG_INFO(...) VPRINTF(LOW, "MCU:" #__VA_ARGS__)
+#define LOG_ERROR(...) VPRINTF(LOW, "MCU ERROR:" #__VA_ARGS__);
+#define LOG_INFO(...) VPRINTF(LOW, "MCU:" #__VA_ARGS__);
 
 // This struct is used to store the partition information used in the test.
 typedef struct partition_info {

src/integration/test_suites/smoke_test_fuse_prov_with_axi_id/smoke_test_fuse_prov_with_axi_id.c
@@ -469,7 +469,7 @@ void main (void) {
     uint32_t mbox_resp_dlen;
     uint32_t mbox_resp_data;
     uint32_t cptra_boot_go;
-    VPRINTF(LOW, "=================\nMCU Caliptra Boot Go\n=================\n\n")
+    VPRINTF(LOW, "=================\nMCU Caliptra Boot Go\n=================\n\n");
     
     // Writing to Caliptra Boot GO register of MCI for CSS BootFSM to bring Caliptra out of reset 
     // This is just to see CSSBootFSM running correctly

commit df2a2e76166011dcbcd693883f6c3e0b2556c3a8

lowRISC change bundle 4 (#801)
* [fuse_ctrl,dv] Fix build of caliptra_ss_fuse_ctrl_test_unlocked0_prov

This accidentally got broken in 5cc9dbe.

* [fuse_ctrl,sw] Correct the no. of fuses reported in fuse_ctrl_mmap.h

Also add an explicit documentation comment in the header file that
explains how "num_fuses" relates to what is specified in the hjson
file: it took me a while to figure it all out.

* [fuse_ctrl,dv] Fix broken logs in smoke_test_fc_cptra_zeroization

* [fuse_ctrl,dv] Improve prints in caliptra_ss_fuse_ctrl_init_fail

No functional change, but the debug messages should be a lot clearer.

* [fuse_ctrl,dv] Remove stray declaration of sw_transition_req

This is also declared in lc_ctrl.h (where it belongs). Don't duplicate
the declaration here.

* [dv] Remove duplication of sw_transition_req

This is in lc_ctrl.h, so there's no need to have a copy on
smoke_test_fuse_prov_with_axi_id.

* MICROSOFT AUTOMATED PIPELINE: Stamp 'lowrisc_4' with updated timestamp and hash after successful run

.github/workflow_metadata/pr_hash
@@ -1,1 +1,1 @@
-dc4ca38081b5ee671de6f27ee548371a5a6d957f6f60016311c05e646d8e3c03b2b9f7160d328e31c7989c7035eac9e2
+ab7f7e70b1cac638d8a45738bb7ac8552aea4a18db38f8e80865271796686829aa532399a369e302af9183a0f2cd888d

.github/workflow_metadata/pr_timestamp
@@ -1,1 +1,1 @@
-1760037784
+1760044200

src/fuse_ctrl/templates/fuse_ctrl_mmap.c.tpl
@@ -53,7 +53,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = ${"true" if p["integrity"] else "false"},
         .lc_phase = ${lc_state_decode(p["lc_phase"])},
         .is_lifecycle = ${"true" if p["variant"] == "LifeCycle" else "false"},
-        .num_fuses = ${len(p["items"])-1},
+        .num_fuses = ${len(p["items"])},
         .fuses = ${p["name"].lower() + "_fuses"}
     },
 % endfor

src/fuse_ctrl/templates/fuse_ctrl_mmap.h.tpl
@@ -53,6 +53,10 @@ typedef struct {
     bool            has_ecc;
     uint32_t        lc_phase;
     bool            is_lifecycle;
+    // The number of fuses in the partition. Note that this may be
+    // larger than the number of fuses explictly specified in the
+    // hjson. Any digest or zeroization field is also added to the
+    // list.
     uint32_t        num_fuses;
     const uint32_t *fuses;
 } partition_t;

src/integration/rtl/fuse_ctrl_mmap.c
@@ -30,7 +30,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 16,
         .is_lifecycle = false,
-        .num_fuses = 1,
+        .num_fuses = 2,
         .fuses = sw_test_unlock_partition_fuses
     },
     // SECRET_MANUF_PARTITION
@@ -47,7 +47,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 16,
         .is_lifecycle = false,
-        .num_fuses = 2,
+        .num_fuses = 3,
         .fuses = secret_manuf_partition_fuses
     },
     // SECRET_PROD_PARTITION_0
@@ -64,7 +64,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 2,
+        .num_fuses = 3,
         .fuses = secret_prod_partition_0_fuses
     },
     // SECRET_PROD_PARTITION_1
@@ -81,7 +81,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 2,
+        .num_fuses = 3,
         .fuses = secret_prod_partition_1_fuses
     },
     // SECRET_PROD_PARTITION_2
@@ -98,7 +98,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 2,
+        .num_fuses = 3,
         .fuses = secret_prod_partition_2_fuses
     },
     // SECRET_PROD_PARTITION_3
@@ -115,7 +115,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 2,
+        .num_fuses = 3,
         .fuses = secret_prod_partition_3_fuses
     },
     // SW_MANUF_PARTITION
@@ -132,7 +132,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 16,
         .is_lifecycle = false,
-        .num_fuses = 13,
+        .num_fuses = 14,
         .fuses = sw_manuf_partition_fuses
     },
     // SECRET_LC_TRANSITION_PARTITION
@@ -149,7 +149,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 1,
         .is_lifecycle = false,
-        .num_fuses = 11,
+        .num_fuses = 12,
         .fuses = secret_lc_transition_partition_fuses
     },
     // SVN_PARTITION
@@ -166,7 +166,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = false,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 3,
+        .num_fuses = 4,
         .fuses = svn_partition_fuses
     },
     // VENDOR_TEST_PARTITION
@@ -183,7 +183,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = false,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 1,
+        .num_fuses = 2,
         .fuses = vendor_test_partition_fuses
     },
     // VENDOR_HASHES_MANUF_PARTITION
@@ -200,7 +200,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = false,
         .lc_phase = 16,
         .is_lifecycle = false,
-        .num_fuses = 2,
+        .num_fuses = 3,
         .fuses = vendor_hashes_manuf_partition_fuses
     },
     // VENDOR_HASHES_PROD_PARTITION
@@ -217,7 +217,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = false,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 34,
+        .num_fuses = 35,
         .fuses = vendor_hashes_prod_partition_fuses
     },
     // VENDOR_REVOCATIONS_PROD_PARTITION
@@ -234,7 +234,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = false,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 51,
+        .num_fuses = 52,
         .fuses = vendor_revocations_prod_partition_fuses
     },
     // VENDOR_SECRET_PROD_PARTITION
@@ -251,7 +251,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 17,
+        .num_fuses = 18,
         .fuses = vendor_secret_prod_partition_fuses
     },
     // VENDOR_NON_SECRET_PROD_PARTITION
@@ -268,7 +268,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 16,
+        .num_fuses = 17,
         .fuses = vendor_non_secret_prod_partition_fuses
     },
     // CPTRA_SS_LOCK_HEK_PROD_0
@@ -285,7 +285,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 2,
+        .num_fuses = 3,
         .fuses = cptra_ss_lock_hek_prod_0_fuses
     },
     // CPTRA_SS_LOCK_HEK_PROD_1
@@ -302,7 +302,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 2,
+        .num_fuses = 3,
         .fuses = cptra_ss_lock_hek_prod_1_fuses
     },
     // CPTRA_SS_LOCK_HEK_PROD_2
@@ -319,7 +319,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 2,
+        .num_fuses = 3,
         .fuses = cptra_ss_lock_hek_prod_2_fuses
     },
     // CPTRA_SS_LOCK_HEK_PROD_3
@@ -336,7 +336,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 2,
+        .num_fuses = 3,
         .fuses = cptra_ss_lock_hek_prod_3_fuses
     },
     // CPTRA_SS_LOCK_HEK_PROD_4
@@ -353,7 +353,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 2,
+        .num_fuses = 3,
         .fuses = cptra_ss_lock_hek_prod_4_fuses
     },
     // CPTRA_SS_LOCK_HEK_PROD_5
@@ -370,7 +370,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 2,
+        .num_fuses = 3,
         .fuses = cptra_ss_lock_hek_prod_5_fuses
     },
     // CPTRA_SS_LOCK_HEK_PROD_6
@@ -387,7 +387,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 2,
+        .num_fuses = 3,
         .fuses = cptra_ss_lock_hek_prod_6_fuses
     },
     // CPTRA_SS_LOCK_HEK_PROD_7
@@ -404,7 +404,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 17,
         .is_lifecycle = false,
-        .num_fuses = 2,
+        .num_fuses = 3,
         .fuses = cptra_ss_lock_hek_prod_7_fuses
     },
     // LIFE_CYCLE
@@ -421,7 +421,7 @@ const partition_t partitions[NUM_PARTITIONS] = {
         .has_ecc = true,
         .lc_phase = 0,
         .is_lifecycle = true,
-        .num_fuses = 1,
+        .num_fuses = 2,
         .fuses = life_cycle_fuses
     },
 };

src/integration/rtl/fuse_ctrl_mmap.h
@@ -250,6 +250,10 @@ typedef struct {
     bool            has_ecc;
     uint32_t        lc_phase;
     bool            is_lifecycle;
+    // The number of fuses in the partition. Note that this may be
+    // larger than the number of fuses explictly specified in the
+    // hjson. Any digest or zeroization field is also added to the
+    // list.
     uint32_t        num_fuses;
     const uint32_t *fuses;
 } partition_t;

src/integration/test_suites/caliptra_ss_fuse_ctrl_init_fail/caliptra_ss_fuse_ctrl_init_fail.c
@@ -84,22 +84,16 @@ void init_fail() {
 
     uint32_t exp_dai_error = (fault == CMD_FC_LCC_CORRECTABLE_FAULT) ? single_error : uncor_error;
 
-    wait_dai_op_idle(exp_dai_error);
+    if (!wait_dai_op_idle(exp_dai_error)) return;
 
-    uint32_t err_reg = lsu_read_32(SOC_OTP_CTRL_ERR_CODE_RF_ERR_CODE_0 + 0x4*partition.index);
-    uint32_t status_reg = lsu_read_32(SOC_OTP_CTRL_STATUS);
+    const char *err_desc = ((fault == CMD_FC_LCC_CORRECTABLE_FAULT) ?
+                            "a correctable" : "an uncorrectable");
+    unsigned exp_err_code = (fault == CMD_FC_LCC_CORRECTABLE_FAULT) ? 2 : 3;
+    uint32_t err_code = lsu_read_32(SOC_OTP_CTRL_ERR_CODE_RF_ERR_CODE_0 + 0x4*partition.index);
 
-    if (fault == CMD_FC_LCC_CORRECTABLE_FAULT) {
-        if (err_reg != 0x2) {
-            VPRINTF(LOW, "ERROR: err code register does not show correctable error: %08X\n", err_reg);
-            return;
-        }
-    } else {
-        if (err_reg != 0x3) {
-            VPRINTF(LOW, "ERROR: err code register does not show uncorrectable error: %08X\n", err_reg);
-            return;
-        }
-    }
+    if (err_code != exp_err_code)
+        VPRINTF(LOW, "ERROR: After %s error, the error code register has %d but we expect %d.\n",
+                err_desc, err_code, exp_err_code);
 }
 
 void main (void) {

src/integration/test_suites/caliptra_ss_fuse_ctrl_test_unlocked0_prov/caliptra_ss_fuse_ctrl_test_unlocked0_prov.c
@@ -51,7 +51,7 @@ void test_unlocked0_provision() {
     uint32_t rnd_fuse_addresses[NUM_PARTITIONS-1];
 
     for (uint32_t i = 0; i < (NUM_PARTITIONS-1); i++) {
-        if (is_caliptra_secret_addr(part_sel[i].address)) {
+        if (is_caliptra_secret_addr(partitions[i].address)) {
             grant_caliptra_core_for_fc_writes();
         } else {
             grant_mcu_for_fc_writes(); 
@@ -77,7 +77,7 @@ void test_unlocked0_provision() {
     wait_dai_op_idle(0);
 
     for (uint32_t i = 0; i < (NUM_PARTITIONS-1); i++) {
-        if (is_caliptra_secret_addr(part_sel[i].address)) {
+        if (is_caliptra_secret_addr(partitions[i].address)) {
             grant_caliptra_core_for_fc_writes();
         } else {
             grant_mcu_for_fc_writes(); 

src/integration/test_suites/libs/fuse_ctrl/fuse_ctrl.h
@@ -24,12 +24,6 @@ void grant_mcu_for_fc_writes(void);
 void revoke_grant_mcu_for_fc_writes(void);
 void grant_caliptra_core_for_fc_writes(void);
 void initialize_otp_controller(void);
-void sw_transition_req(uint32_t next_lc_state,
-                        uint32_t token_127_96,
-                        uint32_t token_95_64,
-                        uint32_t token_63_32,
-                        uint32_t token_31_0,
-                        uint32_t conditional);
 void disable_fc_all_ones_sva(void);
 void enable_fc_all_ones_sva(void);
 

src/integration/test_suites/smoke_test_fc_cptra_zeroization/smoke_test_fc_cptra_zeroization.c
@@ -33,9 +33,6 @@ enum printf_verbosity verbosity_g = CPT_VERBOSITY;
 enum printf_verbosity verbosity_g = LOW;
 #endif
 
-#define LOG_ERROR(...) VPRINTF(LOW, "MCU ERROR:" #__VA_ARGS__);
-#define LOG_INFO(...) VPRINTF(LOW, "MCU:" #__VA_ARGS__);
-
 // This struct is used to store the partition information used in the test.
 typedef struct partition_info {
   // Partition identifier. Used to access partition in the partitions array.
@@ -113,7 +110,7 @@ void main(void) {
   // Releases the Caliptra core by setting CPTRA_FUSE_WR_DONE.
   lsu_write_32(SOC_SOC_IFC_REG_CPTRA_FUSE_WR_DONE,
                SOC_IFC_REG_CPTRA_FUSE_WR_DONE_DONE_MASK);
-  LOG_INFO("Set FUSE_WR_DONE\n");
+  VPRINTF(LOW, "MCU: Set FUSE_WR_DONE\n");
 
   mcu_cptra_advance_brkpoint();
 
@@ -144,7 +141,7 @@ void main(void) {
   // At this point, the partitions should be zeroized.
   for (uint32_t i = 0; i < kNumPartitions; i++) {
     if (!check_digest(&kPartitionsInfo[i], /*expected_zeroized=*/true)) {
-      LOG_ERROR("Partition %d is not zeroized\n", kPartitionsInfo[i].id);
+      VPRINTF(LOW, "MCU ERROR: Partition %d is not zeroized\n", kPartitionsInfo[i].id);
       break;
     }
   }

src/integration/test_suites/smoke_test_fuse_prov_with_axi_id/smoke_test_fuse_prov_with_axi_id.c
@@ -19,6 +19,7 @@
 #include "printf.h"
 #include "riscv_hw_if.h"
 #include "soc_ifc.h"
+#include "lc_ctrl.h"
 #include "caliptra_ss_lc_ctrl_address_map.h"
 #include <string.h>
 #include <stdint.h>
@@ -63,84 +64,6 @@ void lcc_initilization(){
     }
     VPRINTF(LOW, "LC_CTRL: CALIPTRA_SS_LC_CTRL is initalized!\n");
     
-}
-
-void sw_transition_req(uint32_t next_lc_state,
-                        uint32_t token_127_96,
-                        uint32_t token_95_64,
-                        uint32_t token_63_32,
-                        uint32_t token_31_0,
-                        uint32_t conditional) {
-    uint32_t reg_value;
-    uint32_t status_val;
-    uint32_t loop_ctrl;
-    int trigger_alert = 0;
-
-    VPRINTF(LOW, "Starting sw_transition_req...\n");
-
-    // Step 1: Set Claim Transition Register
-    loop_ctrl = 0;
-    while (loop_ctrl != CLAIM_TRANS_VAL) {
-        lsu_write_32(LC_CTRL_CLAIM_TRANSITION_IF_OFFSET, CLAIM_TRANS_VAL);
-        reg_value = lsu_read_32(LC_CTRL_CLAIM_TRANSITION_IF_OFFSET);
-        loop_ctrl = reg_value & CLAIM_TRANS_VAL;
-        VPRINTF(LOW, "Claim Mutex Register [0x%08x]: Read 0x%08x, expected 0x%08x\n",
-                LC_CTRL_CLAIM_TRANSITION_IF_OFFSET, reg_value, CLAIM_TRANS_VAL);
-    }
-    VPRINTF(LOW, "LC_CTRL: Mutex successfully acquired.\n");
-
-    // Step 3: Set Target Lifecycle State
-    VPRINTF(LOW, "Setting next lifecycle state [0x%08x]: 0x%08x\n", LC_CTRL_TRANSITION_TARGET_OFFSET, next_lc_state);
-    lsu_write_32(LC_CTRL_TRANSITION_TARGET_OFFSET, next_lc_state);
-
-    // Step 4: Write Transition Tokens
-    if (conditional == 1) {        
-        VPRINTF(LOW, "Writing tokens: 0x%08x\n", token_127_96);
-        lsu_write_32(LC_CTRL_TRANSITION_TOKEN_0_OFFSET, token_127_96);
-        VPRINTF(LOW, "Writing tokens: 0x%08x\n", token_95_64);
-        lsu_write_32(LC_CTRL_TRANSITION_TOKEN_0_OFFSET, token_95_64);
-        VPRINTF(LOW, "Writing tokens: 0x%08x\n", token_63_32);
-        lsu_write_32(LC_CTRL_TRANSITION_TOKEN_0_OFFSET, token_63_32);
-        VPRINTF(LOW, "Writing tokens: 0x%08x\n", token_31_0);
-        lsu_write_32(LC_CTRL_TRANSITION_TOKEN_0_OFFSET, token_31_0);
-    }
-
-    // Step 6: Trigger the Transition Command
-    VPRINTF(LOW, "Triggering transition command [0x%08x]: 0x1\n", LC_CTRL_TRANSITION_CMD_OFFSET);
-    lsu_write_32(LC_CTRL_TRANSITION_CMD_OFFSET, 0x1);
-
-    // Step 7: Poll Status Register
-    VPRINTF(LOW, "Polling status register [0x%08x]...\n", LC_CTRL_STATUS_OFFSET);
-    while (1) {
-        status_val = lsu_read_32(LC_CTRL_STATUS_OFFSET);
-        uint32_t TRANSITION_SUCCESSFUL = ((status_val & 0x8) >> 3);
-        uint32_t TOKEN_ERROR = ((status_val & 0x40) >> 6);
-        uint32_t OTP_ERROR = ((status_val & 0x100) >> 8);
-        uint32_t RMA_ERROR = ((status_val & 0x80) >> 7);
-
-        VPRINTF(LOW, "Status Register: 0x%08x | Transition Successful: %d | Token Error: %d | OTP Error: %d\n",
-                status_val, TRANSITION_SUCCESSFUL, TOKEN_ERROR, OTP_ERROR);
-
-        if (TRANSITION_SUCCESSFUL) {
-            VPRINTF(LOW, "Transition successful.\n");
-            break;
-        }
-        if (TOKEN_ERROR) {
-            VPRINTF(LOW, "Token error detected.\n");
-            break;
-        }
-        if (OTP_ERROR) {
-            VPRINTF(LOW, "OTP error detected.\n");
-            break;
-        }
-        if (RMA_ERROR) {
-            VPRINTF(LOW, "FLASH RMA error detected.\n");
-            break;
-        }
-    }
-    lsu_write_32(LC_CTRL_CLAIM_TRANSITION_IF_OFFSET, 0x0);
-
-    VPRINTF(LOW, "sw_transition_req completed.\n");
 }
 
 void RAW_to_TESTUNLOCK0(){