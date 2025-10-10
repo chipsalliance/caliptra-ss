70c1380041ca8f4fadb068e4f6d4d6a91ef2dc33
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-10-10 22:55:25 +0100

    [fuse_ctrl,dv] Fixes to caliptra_ss_fuse_ctrl_init_fail
    There were several problems with this test. Firstly, we sometimes
    polled the OTP status too early, and could end up seeing a large fault
    cause a single partition error before it got mirrored to the other
    partitions.
    
    The expected value of the error code register is also modelled wrongly
    for an uncorrectable error: the ECC failure that it causes actually
    sets the error code to 6 (CHECK_FAIL_ERROR) instead of
    3 (MACRO_ECC_UNCORR_ERROR).
    
    This commit also adds some "early exits on failure" to avoid wasting
    so much time if something has gone wrong.

--- src/integration/test_suites/caliptra_ss_fuse_ctrl_init_fail/caliptra_ss_fuse_ctrl_init_fail.c
+++ src/integration/test_suites/caliptra_ss_fuse_ctrl_init_fail/caliptra_ss_fuse_ctrl_init_fail.c
@@ -36,7 +36,19 @@ volatile char* stdout = (char *)SOC_MCI_TOP_MCI_REG_DEBUG_OUT;
     enum printf_verbosity verbosity_g = LOW;
 #endif
 
-void init_fail() {
+static void nop_sleep(unsigned count) {
+    for (unsigned ii = 0; ii < count; ii++) {
+        __asm__ volatile ("nop"); // Sleep loop as "nop"
+    }
+}
+
+bool body(void) {
+    if (!transition_state(TEST_UNLOCKED0, raw_unlock_token)) return false;
+
+    if (!wait_dai_op_idle(0)) return false;
+
+    initialize_otp_controller();
+
     const uint32_t faults[2] = {
         CMD_FC_LCC_CORRECTABLE_FAULT,
         CMD_FC_LCC_UNCORRECTABLE_FAULT
@@ -55,6 +67,7 @@ void init_fail() {
 
     partition_t partition = part_sel[xorshift32() % count];
     uint32_t fault = faults[xorshift32() % 2];
+    bool is_correctable = (fault == CMD_FC_LCC_CORRECTABLE_FAULT);
 
     if (is_caliptra_secret_addr(partition.address)) {
         grant_caliptra_core_for_fc_writes();
@@ -63,39 +76,89 @@ void init_fail() {
     }
 
     // Write one word in the selected partition and lock it afterwards.
-    dai_wr(partition.address, 0x1, 0x2, partition.granularity, 0);
+    if (!dai_wr(partition.address, 0x1, 0x2, partition.granularity, 0)) return false;
     calculate_digest(partition.address, 0);
 
     // Inject either a correctable or uncorrectable error into all locked partitions with digests
     // (as recorded in a list in fc_lcc_tb_services). In particular, this will include the selected
     // partition.
+    const char *err_desc = is_correctable ? "a correctable" : "an uncorrectable";
+    VPRINTF(LOW, "INFO: About to inject %s fault into partition %d.\n", err_desc, partition.index);
+
     lsu_write_32(SOC_MCI_TOP_MCI_REG_DEBUG_OUT, fault);
-    wait_dai_op_idle(0);
+    if (!wait_dai_op_idle(0)) return false;
 
+    // Reset lc_ctrl. If there's a partition error, we expect it to percolate up to the status
+    // register very quickly but that error will take some time to "upgrade" if this is an
+    // uncorrectable error. Give the block another 1e3 cycles (which, from experiment, should be a
+    // generous upper bound).
     reset_fc_lcc_rtl();
+    nop_sleep(1000);
 
-    // If the fault was correctable, we expect the DAI to stop with an error on the bit
-    // corresponding to the partition (which will be 1 << partition.index). If it was uncorrectable,
-    // we expect the DAI to stop with all partition error bits set, together with DAI_ERROR and
-    // LCI_ERROR.
-    uint32_t single_error = 1 << partition.index;
+    // At this point, we want to predict the value of OTP_CTRL_STATUS that we expect. This is a bit
+    // tricky, because several of the partition bits depend on lots that we don't really want to
+    // model here. BUT:
+    //
+    //    1. We always expect the error bit for the targeted partition (1 << partition.index) to be
+    //       set.
+    //
+    //    2. For an uncorrectable error, we expect all partition error bits set, together with
+    //       DAI_ERROR and LCI_ERROR.
+    //
+    //    3. For a correctable error, it's a bit tricky to predict which other partitions' error
+    //       bits will be set. But we *do* know that DAI_ERROR and LCI_ERROR are expected to be
+    //       false.
+
+    uint32_t the_part_error = (1 << partition.index);
     uint32_t all_part_errors = (1 << NUM_PARTITIONS) - 1;
-    uint32_t uncor_error = (all_part_errors |
-                            OTP_CTRL_STATUS_DAI_ERROR_MASK |
-                            OTP_CTRL_STATUS_LCI_ERROR_MASK);
+    uint32_t top_error_bits = (OTP_CTRL_STATUS_DAI_ERROR_MASK |
+                               OTP_CTRL_STATUS_LCI_ERROR_MASK);
 
-    uint32_t exp_dai_error = (fault == CMD_FC_LCC_CORRECTABLE_FAULT) ? single_error : uncor_error;
+    uint32_t error_req = 0, error_opt = 0;
+    if (is_correctable) {
+        error_req = the_part_error;
+        error_opt = all_part_errors;
+    } else {
+        error_req = all_part_errors | top_error_bits;
+    }
 
-    if (!wait_dai_op_idle(exp_dai_error)) return;
+    // We could use the wait_dai_op_idle function to wait until the DAI is idle or an error happens,
+    // but we expect an error to have appeared by now (and, if it is uncorrectable, the DAI will
+    // never become idle).
+    //
+    // Instead of doing that, read the status register directly, then mask out the IDLE bit in case
+    // it was set.
+    uint32_t otp_status = lsu_read_32(SOC_OTP_CTRL_STATUS) & ~OTP_CTRL_STATUS_DAI_IDLE_MASK;
+    bool success = true;
 
-    const char *err_desc = ((fault == CMD_FC_LCC_CORRECTABLE_FAULT) ?
-                            "a correctable" : "an uncorrectable");
-    unsigned exp_err_code = (fault == CMD_FC_LCC_CORRECTABLE_FAULT) ? 2 : 3;
+    if (error_req & ~otp_status) {
+        VPRINTF(LOW,
+                "ERROR: After %s error, not enough error bits are set. "
+                "required 0x%08x; status = 0x%08x => missing: 0x%08x.\n",
+                err_desc, error_req, otp_status, error_req & ~otp_status);
+        success = false;
+    }
+    if (otp_status & ~(error_req | error_opt)) {
+        VPRINTF(LOW,
+                "ERROR: After %s error, extra error bits are set. "
+                "status = 0x%08x; allowed = 0x%08x => extra: 0x%08x.\n",
+                err_desc, otp_status, error_req | error_opt, otp_status & ~(error_req | error_opt));
+        success = false;
+    }
+
+    // Predict the error code. It should be 2 (MacroEccCorrError) on a correctable error but an
+    // uncorrectable error will actually cause an FSM fault, which causes all three bits of the
+    // error code to go high (giving 7).
+    unsigned exp_err_code = is_correctable ? 2 : 7;
     uint32_t err_code = lsu_read_32(SOC_OTP_CTRL_ERR_CODE_RF_ERR_CODE_0 + 0x4*partition.index);
 
-    if (err_code != exp_err_code)
+    if (err_code != exp_err_code) {
         VPRINTF(LOW, "ERROR: After %s error, the error code register has %d but we expect %d.\n",
                 err_desc, err_code, exp_err_code);
+        success = false;
+    }
+
+    return success;
 }
 
 void main (void) {
@@ -107,16 +170,9 @@ void main (void) {
     lcc_initialization();
     grant_mcu_for_fc_writes(); 
 
-    transition_state(TEST_UNLOCKED0, raw_unlock_token);
-    wait_dai_op_idle(0);
+    bool test_passed = body();
 
-    initialize_otp_controller();
+    nop_sleep(160);
 
-    init_fail();
-
-    for (uint8_t ii = 0; ii < 160; ii++) {
-        __asm__ volatile ("nop"); // Sleep loop as "nop"
-    }
-
-    SEND_STDOUT_CTRL(0xff);
+    SEND_STDOUT_CTRL(test_passed ? TB_CMD_TEST_PASS : TB_CMD_TEST_FAIL);
 }
