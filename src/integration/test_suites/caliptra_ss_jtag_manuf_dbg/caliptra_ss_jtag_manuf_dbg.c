11fc5d54a4d85f436c52d4d983188d3d3664b3e8
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-10-17 19:29:28 +0100

    [dv] Rather shorten caliptra_ss_jtag_manuf_dbg.c
    Remove various bits of unused code, and pull the two obvious loops out
    into functions that can be given sensible names.

--- src/integration/test_suites/caliptra_ss_jtag_manuf_dbg/caliptra_ss_jtag_manuf_dbg.c
+++ src/integration/test_suites/caliptra_ss_jtag_manuf_dbg/caliptra_ss_jtag_manuf_dbg.c
@@ -1,6 +1,6 @@
 //********************************************************************************
 // SPDX-License-Identifier: Apache-2.0
-// 
+//
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 /*
     Run this test with the following command
     submit_i cpt_build2 -tc caliptra_ss_jtag_manuf_dbg -op -to 5000000
-    or 
+    or
     submit_i cpt_build -tc caliptra_ss_jtag_manuf_dbg -op -to 5000000
 */
 
@@ -39,33 +39,24 @@ volatile char* stdout = (char *)SOC_MCI_TOP_MCI_REG_DEBUG_OUT;
     enum printf_verbosity verbosity_g = LOW;
 #endif
 
+void wait_for_mask(uint32_t addr, uint32_t mask)
+{
+    uint32_t seen = 0;
+    do {
+        seen = lsu_read_32(addr);
+    } while (mask & ~seen);
+}
 
-
+void write_256(uint32_t base_address, const uint32_t vector[16])
+{
+    uint32_t addr = base_address;
+    for (int i = 0; i < 16; i++, addr += 4) {
+        VPRINTF(LOW, "MCU: writing 0x%x to 0x%x\n", vector[i], addr);
+        lsu_write_32(addr, vector[i]);
+    }
+}
 
 void main (void) {
-    int argc=0;
-    char *argv[1];
-    enum boot_fsm_state_e boot_fsm_ps;
-    const uint32_t mbox_dlen = 64;
-    uint32_t mbox_data[] = { 0x00000000,
-                             0x11111111,
-                             0x22222222,
-                             0x33333333,
-                             0x44444444,
-                             0x55555555,
-                             0x66666666,
-                             0x77777777,
-                             0x88888888,
-                             0x99999999,
-                             0xaaaaaaaa,
-                             0xbbbbbbbb,
-                             0xcccccccc,
-                             0xdddddddd,
-                             0xeeeeeeee,
-                             0xffffffff };
-    uint32_t mbox_resp_dlen;
-    uint32_t mbox_resp_data;
-    uint32_t cptra_boot_go;
     // New step: Writing the 16-word hash(MANUF_DBG_TOKEN) with byte swapping
     uint32_t vector[16] = {
         0xba03f195, 0xcfb86f60, 0x6ce5adc0, 0x90be97cd,
@@ -73,59 +64,39 @@ void main (void) {
         0x28abfbfa, 0x5a8f41, 0x44901cee, 0x4961df3f,
         0x8db3e5ea, 0x8d489c51, 0x90e26b42, 0xaf9369e
     };
-    
 
-
-    // VPRINTF(LOW, "=================\nMCU Caliptra Boot Go\n=================\n\n");
-    
-    // Writing to Caliptra Boot GO register of MCI for CSS BootFSM to bring Caliptra out of reset 
+    // Writing to Caliptra Boot GO register of MCI for CSS BootFSM to bring Caliptra out of reset
     // This is just to see CSSBootFSM running correctly
     lsu_write_32(SOC_MCI_TOP_MCI_REG_CPTRA_BOOT_GO, 1);
     VPRINTF(LOW, "MCU: Writing MCI SOC_MCI_TOP_MCI_REG_CALIPTRA_BOOT_GO\n");
 
-    cptra_boot_go = lsu_read_32(SOC_MCI_TOP_MCI_REG_CPTRA_BOOT_GO);
+    uint32_t cptra_boot_go = lsu_read_32(SOC_MCI_TOP_MCI_REG_CPTRA_BOOT_GO);
     VPRINTF(LOW, "MCU: Reading SOC_MCI_TOP_MCI_REG_CALIPTRA_BOOT_GO %x\n", cptra_boot_go);
 
     ////////////////////////////////////
     // Fuse and Boot Bringup
     //
     // Wait for ready_for_fuses
-    while(!(lsu_read_32(SOC_SOC_IFC_REG_CPTRA_FLOW_STATUS) & SOC_IFC_REG_CPTRA_FLOW_STATUS_READY_FOR_FUSES_MASK));
+    wait_for_mask(SOC_SOC_IFC_REG_CPTRA_FLOW_STATUS,
+                  SOC_IFC_REG_CPTRA_FLOW_STATUS_READY_FOR_FUSES_MASK);
 
     // Initialize fuses
-        uint32_t base_address = SOC_SOC_IFC_REG_FUSE_MANUF_DBG_UNLOCK_TOKEN_0;
-    for (int i = 0; i < 16; i++) {
-        // uint32_t swapped = ((vector[i] & 0xFF) << 24) |
-        //                    ((vector[i] & 0xFF00) << 8) |
-        //                    ((vector[i] & 0xFF0000) >> 8) |
-        //                    ((vector[i] & 0xFF000000) >> 24);
-        VPRINTF(LOW, "MCU: writing 0x%x to address of 0x%x\n", vector[i], base_address + (i * 4));
-        lsu_write_32(base_address + (i * 4), vector[i]);
-    }
+    write_256(SOC_SOC_IFC_REG_FUSE_MANUF_DBG_UNLOCK_TOKEN_0, vector);
 
     lsu_write_32(SOC_SOC_IFC_REG_CPTRA_FUSE_WR_DONE, SOC_IFC_REG_CPTRA_FUSE_WR_DONE_DONE_MASK);
     VPRINTF(LOW, "MCU: Set fuse wr done\n");
 
-    for (uint32_t ii = 0; ii < 5000; ii++) {
-        __asm__ volatile ("nop"); // Sleep loop as "nop"
-    }
-    
+    mcu_sleep(5000);
+
     VPRINTF(LOW, "=================\n CALIPTRA_SS JTAG MANUF DEBUG FLOW TEST with ROM \n=================\n\n");
-    
 
-    for (uint32_t ii = 0; ii < 5000; ii++) {
-        __asm__ volatile ("nop"); // Sleep loop as "nop"
-    }
+    mcu_sleep(5000);
 
-    cptra_boot_go = 0;
     VPRINTF(LOW, "MCU: waits in success loop\n");
-    while(cptra_boot_go != SOC_IFC_REG_SS_DBG_MANUF_SERVICE_REG_RSP_MANUF_DBG_UNLOCK_SUCCESS_MASK){
-        cptra_boot_go = lsu_read_32(SOC_SOC_IFC_REG_SS_DBG_MANUF_SERVICE_REG_RSP) & SOC_IFC_REG_SS_DBG_MANUF_SERVICE_REG_RSP_MANUF_DBG_UNLOCK_SUCCESS_MASK;
-    }    
-    for (uint32_t ii = 0; ii < 5000; ii++) {
-        __asm__ volatile ("nop"); // Sleep loop as "nop"
-    }
+    wait_for_mask(SOC_SOC_IFC_REG_SS_DBG_MANUF_SERVICE_REG_RSP,
+                  SOC_IFC_REG_SS_DBG_MANUF_SERVICE_REG_RSP_MANUF_DBG_UNLOCK_SUCCESS_MASK);
 
-    SEND_STDOUT_CTRL(0xff);
+    mcu_sleep(5000);
 
+    SEND_STDOUT_CTRL(TB_CMD_TEST_PASS);
 }
