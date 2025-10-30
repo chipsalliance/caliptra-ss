chipsalliance/caliptra-ss

commit baf339f659d833bd3ef90f9b2784afb8937962b6

[fuse_ctrl,dv] Assorted cleanups to how FC tests look at magic registers (#750)
* [fuse_ctrl] Create fuse_ctrl_mmap.c

This avoids fuse_ctrl_mmap.h defining something that actually gets
instantiated (and defines a symbol). Without fixing that, if more than
one compilation unit includes the header then link will fail because
more than one object file defines "partitions" and various fuse index
arrays.

* [fuse_ctrl] Add some documentation for address range type in package

No functional change (but hopefully it makes things a bit clearer: I
found these ranges a bit hard to reason about).

* [fuse_ctrl,dv] Tidy up tests for Caliptra secret addresses

The value in caliptra_ss_fuse_ctrl_manuf_prod_prov hadn't yet been
updated for the change in 275521b1. This change should make it so
that test and also caliptra_ss_fuse_ctrl_init_fail get a magic number
from a single (documented) place.

Apply the same change to caliptra_ss_fuse_ctrl_test_unlocked0_prov.

* [fuse_ctrl,dv] Avoid hard-coding read lock partitions in tests

This is a bit fiddly to do properly, because you end up needing to map
from a partition with a read lock to that partition's read lock
address (which depends on soc_mmap.h).

My solution is to have a template generate that map (which will
contain actual addresses that get pulled from soc_mmap.h at compile
time).

* MICROSOFT AUTOMATED PIPELINE: Stamp 'lowrisc_fuse_ctrl_partition_regs' with updated timestamp and hash after successful run

---------

Co-authored-by: Andreas Kurth (LOWRISC C I C) <v-akurth@microsoft.com>

src/integration/test_suites/caliptra_ss_fuse_ctrl_registers/caliptra_ss_fuse_ctrl_registers.c
@@ -27,6 +27,7 @@
 #include "caliptra_ss_lib.h"
 #include "fuse_ctrl.h"
 #include "lc_ctrl.h"
+#include "fuse_ctrl_read_lock_map.h"
 
 volatile char* stdout = (char *)SOC_MCI_TOP_MCI_REG_DEBUG_OUT;
 #ifdef CPT_VERBOSITY
@@ -34,17 +35,6 @@ volatile char* stdout = (char *)SOC_MCI_TOP_MCI_REG_DEBUG_OUT;
 #else
     enum printf_verbosity verbosity_g = LOW;
 #endif
-
-#define NUM_READ_LOCK_REGS 7
-static uint32_t read_lock_registers[NUM_READ_LOCK_REGS] = {
-    SOC_OTP_CTRL_SW_MANUF_PARTITION_READ_LOCK,
-    SOC_OTP_CTRL_SVN_PARTITION_READ_LOCK,
-    SOC_OTP_CTRL_VENDOR_TEST_PARTITION_READ_LOCK,
-    SOC_OTP_CTRL_VENDOR_HASHES_MANUF_PARTITION_READ_LOCK,
-    SOC_OTP_CTRL_VENDOR_HASHES_PROD_PARTITION_READ_LOCK,
-    SOC_OTP_CTRL_VENDOR_REVOCATIONS_PROD_PARTITION_READ_LOCK,
-    SOC_OTP_CTRL_VENDOR_NON_SECRET_PROD_PARTITION_READ_LOCK
-};
 
 #define NUM_INTER_REGS 3
 static uint32_t interrupt_registers[NUM_INTER_REGS] = {
@@ -122,8 +112,10 @@ void main (void) {
     uint32_t value = 0;
 
     // Make sure all the read lock registers are set to 1 (disabled).
-    for (uint32_t i = 0; i < NUM_READ_LOCK_REGS; i++) {
-        value = lsu_read_32(read_lock_registers[i]);
+    for (int lockable_idx = 0; ; lockable_idx++) {
+        uint32_t partition_idx = read_lock_partition_indices[lockable_idx];
+        if (partition_idx == UINT32_MAX) break;
+        value = lsu_read_32(read_lock_csr_mapping[lockable_idx]);
         if ((value & 0x1) != 0x1) {
             VPRINTF(LOW, "ERROR: incorrect value in read lock register %08X: exp: %08X act: %08X\n", read_lock_registers[i], 0x1, value);
             goto epilogue;

.github/workflow_metadata/pr_hash
@@ -1,1 +1,1 @@
-1b3c181db94e5f427ed60fd70627cf79e8ef63f363706aaf49588fe9f802481eac27b1f15632245035fca76b0369c077
+2e90a36bf5bafa72d6c4662790f22e7610233acd597a1ea55e6cefe223d35b9d5610759feb2e61251e99a8f4abc79d94

.github/workflow_metadata/pr_timestamp
@@ -1,1 +1,1 @@
-1758901881
+1759236346

src/fuse_ctrl/rtl/otp_ctrl_pkg.sv
@@ -371,20 +371,28 @@ package otp_ctrl_pkg;
     DISCARD_FUSE_CMD_AXI_WR_ST = 4'd9
   } fc_table_state_t;
 
-
+  // A structure to represent an address range in the access control table. The bounds are both
+  // inclusive, so a 256 byte window starting at address zero would have lower_addr=0 and
+  // upper_addr=32'hff.
+  //
+  // Note that these are compared with the starting address for a transaction, which works because
+  // all accesses on the interface are the same size (64 bit). As such, the example above should be
+  // encoded with upper_addr=32'f8.
   typedef struct packed {
-    logic [31:0] lower_addr;  // Lower bound of the address range
-    logic [31:0] upper_addr;  // Upper bound of the address range
+    logic [31:0] lower_addr;  // Lower bound
+    logic [31:0] upper_addr;  // Upper bound (inclusive)
   } access_control_entry_t;
   
   localparam int FC_TABLE_NUM_RANGES = 3;
   
   localparam access_control_entry_t access_control_table [FC_TABLE_NUM_RANGES] = '{
-    '{ lower_addr: 32'h00000000, upper_addr: 32'h00003FD8}, // Caliptra core
+    '{ lower_addr: 32'h00000000, upper_addr: 32'h00003FD8},  // Caliptra core
     '{ lower_addr: 32'h000000F8, upper_addr: 32'h00003FD8},  // MCU core
-    '{ lower_addr: 32'h00000000, upper_addr: 32'h00000040}  // MCU core
+    '{ lower_addr: 32'h00000000, upper_addr: 32'h00000040}   // MCU core
   };
 
+  // The range of addresses that are available to the Caliptra core but not the MCU (corresponding
+  // to a gap between two ranges in access_control_table). The upper bound is inclusive.
   localparam int CALIPTRA_SECRET_ACCESS_LOWER_ADDR = 32'h48;
   localparam int CALIPTRA_SECRET_ACCESS_UPPER_ADDR = 32'hF0;
 

src/fuse_ctrl/templates/fuse_ctrl_mmap.c.tpl
@@ -1,0 +1,90 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+<%!
+    def lc_state_decode(state):
+        return ["LcStRaw",
+                "LcStTestUnlocked0", "LcStTestLocked0", "LcStTestUnlocked1", "LcStTestLocked1",
+                "LcStTestUnlocked2", "LcStTestLocked2", "LcStTestUnlocked3", "LcStTestLocked3",
+                "LcStTestUnlocked4", "LcStTestLocked4", "LcStTestUnlocked5", "LcStTestLocked5",
+                "LcStTestUnlocked6", "LcStTestLocked6", "LcStTestUnlocked7",
+                "LcStDev", "LcStProd", "LcStProdEnd", "LcStRma", "LcStScrap"
+                ].index(state)
+%>\
+
+#include "fuse_ctrl_mmap.h"
+
+const partition_t partitions[NUM_PARTITIONS] = {
+% for i, p in enumerate(partitions):
+    // ${p["name"]}
+    {
+        .index = ${i},
+        .address = ${"0x%04X" % p["offset"]},
+% if p["sw_digest"] or p["hw_digest"]:
+  % if p["zeroizable"]:
+        .digest_address = ${"0x%04X" % p["items"][len(p["items"])-2]["offset"]},
+  % else:
+        .digest_address = ${"0x%04X" % p["items"][len(p["items"])-1]["offset"]},
+  % endif
+% else:
+        .digest_address = 0x0000,
+% endif
+% if p["zeroizable"]:
+        .zer_address = ${"0x%04X" % p["items"][len(p["items"])-1]["offset"]},
+% else:
+        .zer_address = 0x0000,
+% endif
+        .variant = ${0 if p["variant"] == "Buffered" else (1 if p["variant"] == "Unbuffered" else 2)},
+        .granularity = ${64 if p["secret"] else 32},
+        .is_secret = ${"true" if p["secret"] else "false"},
+        .hw_digest = ${"true" if p["hw_digest"] else "false"},
+        .sw_digest = ${"true" if p["sw_digest"] else "false"},
+        .has_ecc = ${"true" if p["integrity"] else "false"},
+        .lc_phase = ${lc_state_decode(p["lc_phase"])},
+        .is_lifecycle = ${"true" if p["variant"] == "LifeCycle" else "false"},
+        .num_fuses = ${len(p["items"])-1},
+        .fuses = ${p["name"].lower() + "_fuses"}
+    },
+% endfor
+};
+
+% for i, p in enumerate(partitions[:len(partitions)]):
+const uint32_t ${p["name"].lower()}_fuses[] = {
+  % if (p["hw_digest"] or p["sw_digest"]) and not p["zeroizable"]:
+    % for j, f in enumerate(p["items"][:len(p["items"])-1]):
+      % if j < len(p["items"])-2:
+    ${f["name"]},
+      % else:
+    ${f["name"]}
+      % endif
+    % endfor
+  % elif (p["hw_digest"] or p["sw_digest"]) and p["zeroizable"]:
+    % for j, f in enumerate(p["items"][:len(p["items"])-2]):
+      % if j < len(p["items"])-3:
+    ${f["name"]},
+      % else:
+    ${f["name"]}
+      % endif
+    % endfor
+  % else:
+    % for j, f in enumerate(p["items"][:len(p["items"])]):
+      % if j < len(p["items"])-1:
+    ${f["name"]},
+      % else:
+    ${f["name"]}
+      % endif
+    % endfor
+  % endif
+};
+% endfor

src/fuse_ctrl/templates/fuse_ctrl_mmap.h.tpl
@@ -15,16 +15,10 @@
 
 #ifndef FUSE_CTRL_MMAP_HEADER
 #define FUSE_CTRL_MMAP_HEADERS
-<%!
-    def lc_state_decode(state):
-        return ["LcStRaw",
-                "LcStTestUnlocked0", "LcStTestLocked0", "LcStTestUnlocked1", "LcStTestLocked1",
-                "LcStTestUnlocked2", "LcStTestLocked2", "LcStTestUnlocked3", "LcStTestLocked3",
-                "LcStTestUnlocked4", "LcStTestLocked4", "LcStTestUnlocked5", "LcStTestLocked5",
-                "LcStTestUnlocked6", "LcStTestLocked6", "LcStTestUnlocked7",
-                "LcStDev", "LcStProd", "LcStProdEnd", "LcStRma", "LcStScrap"
-                ].index(state)
-%>
+
+#include <stdbool.h>
+#include <stdint.h>
+
 typedef enum {
 % for i, p in enumerate(partitions):
     // ${p["name"]}
@@ -47,88 +41,30 @@ typedef enum {
 } partition_k;
 
 typedef struct {
-    uint32_t index;
-    uint32_t address;
-    uint32_t digest_address;
-    uint32_t zer_address;
-    uint32_t variant;
-    uint32_t granularity;
-    bool is_secret;
-    bool hw_digest;
-    bool sw_digest;
-    bool has_read_lock;
-    bool has_ecc;
-    bool is_lifecycle;
-    uint32_t lc_phase;
-    uint32_t num_fuses;
-    uint32_t *fuses;
+    uint32_t        index;
+    uint32_t        address;
+    uint32_t        digest_address;
+    uint32_t        zer_address;
+    uint32_t        variant;
+    uint32_t        granularity;
+    bool            is_secret;
+    bool            hw_digest;
+    bool            sw_digest;
+    bool            has_ecc;
+    bool            is_lifecycle;
+    uint32_t        lc_phase;
+    uint32_t        num_fuses;
+    const uint32_t *fuses;
 } partition_t;
 
 #define NUM_PARTITIONS ${len(partitions)}
 
-% for i, p in enumerate(partitions[:len(partitions)]):
-uint32_t ${p["name"].lower()}_fuses[] = {
-  % if (p["hw_digest"] or p["sw_digest"]) and not p["zeroizable"]:
-    % for j, f in enumerate(p["items"][:len(p["items"])-1]):
-      % if j < len(p["items"])-2:
-    ${f["name"]},
-      % else:
-    ${f["name"]}
-      % endif
-    % endfor
-  % elif (p["hw_digest"] or p["sw_digest"]) and p["zeroizable"]:
-    % for j, f in enumerate(p["items"][:len(p["items"])-2]):
-      % if j < len(p["items"])-3:
-    ${f["name"]},
-      % else:
-    ${f["name"]}
-      % endif
-    % endfor
-  % else:
-    % for j, f in enumerate(p["items"][:len(p["items"])]):
-      % if j < len(p["items"])-1:
-    ${f["name"]},
-      % else:
-    ${f["name"]}
-      % endif
-    % endfor
-  % endif
-};
-% endfor
+// A map of the NUM_PARTITIONS fuse partitions.
+extern const partition_t partitions[];
 
-partition_t partitions[NUM_PARTITIONS] = {
-% for i, p in enumerate(partitions):
-    // ${p["name"]}
-    {
-        .index = ${i},
-        .address = ${"0x%04X" % p["offset"]},
-% if p["sw_digest"] or p["hw_digest"]:
-  % if p["zeroizable"]:
-        .digest_address = ${"0x%04X" % p["items"][len(p["items"])-2]["offset"]},
-  % else:
-        .digest_address = ${"0x%04X" % p["items"][len(p["items"])-1]["offset"]},
-  % endif
-% else:
-        .digest_address = 0x0000,
-% endif
-% if p["zeroizable"]:
-        .zer_address = ${"0x%04X" % p["items"][len(p["items"])-1]["offset"]},
-% else:
-        .zer_address = 0x0000,
-% endif
-        .variant = ${0 if p["variant"] == "Buffered" else (1 if p["variant"] == "Unbuffered" else 2)},
-        .granularity = ${64 if p["secret"] else 32},
-        .is_secret = ${"true" if p["secret"] else "false"},
-        .hw_digest = ${"true" if p["hw_digest"] else "false"},
-        .sw_digest = ${"true" if p["sw_digest"] else "false"},
-        .has_read_lock = ${"true" if p["read_lock"] == "CSR" else "false"},
-        .has_ecc = ${"true" if p["integrity"] else "false"},
-        .lc_phase = ${lc_state_decode(p["lc_phase"])},
-        .is_lifecycle = ${"true" if p["variant"] == "LifeCycle" else "false"},
-        .num_fuses = ${len(p["items"])-1},
-        .fuses = ${p["name"].lower() + "_fuses"}
-    },
+// Arrays with indices of fuses for each partition.
+% for i, p in enumerate(partitions[:len(partitions)]):
+extern const uint32_t ${p["name"].lower()}_fuses[];
 % endfor
-};
 
 #endif // FUSE_CTRL_MMAP_HEADER

src/fuse_ctrl/templates/fuse_ctrl_read_lock_map.c.tpl
@@ -1,0 +1,44 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+// NOTE: This file is used for testing in situations where a program running on
+//       the MCU needs to reason about read-lock CSRs. Those CSRs have addresses
+//       defined by the SOC address map. In this file, we pair the two together.
+//
+//       This C file is generated from a template. The template doesn't depend
+//       on SOC-level information but the generated C file references names that
+//       are defined in soc_address_map.h.
+
+#include <stdint.h>
+#include "fuse_ctrl_mmap.h"
+#include "soc_address_map.h"
+
+const uint32_t read_lock_partition_indices[] = {
+% for i, p in enumerate(partitions):
+%  if p["read_lock"] == "CSR":
+    ${i},
+%  endif
+% endfor
+    UINT32_MAX
+};
+
+const uint32_t read_lock_csr_mapping[] = {
+% for i, p in enumerate(partitions):
+%  if p["read_lock"] == "CSR":
+    SOC_OTP_CTRL_${p["name"]}_READ_LOCK,
+%  endif
+% endfor
+    UINT32_MAX
+};

commit d29324cfcafcb0c1574946de18acc5e8cf31b819

[fuse_ctrl,dv] Fix build of caliptra_ss_fuse_ctrl_registers
This was broken by commit baf339f65, where I tidied up the way the
function referenced lackable partitions. Unfortunately, there is a
print message that used the old array name (causing a build failure).

Use the new one.

src/integration/test_suites/caliptra_ss_fuse_ctrl_registers/caliptra_ss_fuse_ctrl_registers.c
@@ -115,7 +115,10 @@ void main (void) {
         if (partition_idx == UINT32_MAX) break;
         value = lsu_read_32(read_lock_csr_mapping[lockable_idx]);
         if ((value & 0x1) != 0x1) {
-            VPRINTF(LOW, "ERROR: incorrect value in read lock register %08X: exp: %08X act: %08X\n", read_lock_registers[i], 0x1, value);
+            VPRINTF(LOW,
+                    ("ERROR: incorrect value in read lock register %08X: "
+                     "exp: %08X act: %08X\n"),
+                    read_lock_csr_mapping[lockable_idx], 0x1, value);
             goto epilogue;
         }
     }