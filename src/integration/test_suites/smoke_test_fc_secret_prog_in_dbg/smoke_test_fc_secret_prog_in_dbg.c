6a089e3d5e31d1c9568b70ab26477c591e34d76b
Emre Karabulut <emre.karabulut.dde@gmail.com>
2025-11-25 09:22:07 -0800

    added missing ;

--- src/integration/test_suites/smoke_test_fc_secret_prog_in_dbg/smoke_test_fc_secret_prog_in_dbg.c
+++ src/integration/test_suites/smoke_test_fc_secret_prog_in_dbg/smoke_test_fc_secret_prog_in_dbg.c
@@ -98,7 +98,7 @@ void program_UDS_FE_secret_prod_partition() {
 }
 
 void main (void) {
-    VPRINTF(LOW, "=================\nMCU Caliptra Boot Go\n=================\n\n")
+    VPRINTF(LOW, "=================\nMCU Caliptra Boot Go\n=================\n\n");
     
     mcu_cptra_init_d();
     wait_dai_op_idle(0);
