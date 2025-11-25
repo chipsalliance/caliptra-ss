a931cd2accde9cdff8b0a773d498a4361658de65
Emre Karabulut <emre.karabulut.dde@gmail.com>
2025-11-25 08:46:36 -0800

    removed the code blocks from old test

--- src/integration/test_suites/smoke_test_fc_secret_prog_in_dbg/smoke_test_fc_secret_prog_in_dbg.c
+++ src/integration/test_suites/smoke_test_fc_secret_prog_in_dbg/smoke_test_fc_secret_prog_in_dbg.c
@@ -1,6 +1,6 @@
 //********************************************************************************
 // SPDX-License-Identifier: Apache-2.0
-//
+// 
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -37,136 +37,83 @@ volatile char* stdout = (char *)SOC_MCI_TOP_MCI_REG_DEBUG_OUT;
 #endif
 
 /**
- * Program a LCC transition token in `VENDOR_SECRET_PROD_PARTITION` zeroize the secret
- * partition and verify that the valid bit is asserted once the partition has been locked.
- * The test proceeds in the following steps:
- *
- *   1. Write a full transition token into the partition.
- *   2. Read back the token and verify it is equal to the token written in Step 1.
- *      This works as long as the secret partition has not been locked.
- *   3. Verify that the partition's digest register is 0.
- *   4. Lock the partition by calculating a hardware digest.
- *   5. Reset the RTL.
- *   6. Try to read the token and verify that it results in an error as the
- *      partition is secret and locked, thus not accessible by software anymore.
- *   7. Try to write a value into the partition and verify that it results in an error
- *      as the partition has been locked in Step 5.
- *   8. Read back the digest from the partition's digest register and verify it
- *      is non-zero.
- *   9. Apply zeroization
- *   10.Verify the valid signal drops via SVA.
+ * Attempt to program `UDS` while caliptra is in debug mode, this prov should fail
+ * 
+ *   1. Write a value to a first UDS address, one of any FE0 address and
+ *      the end of FE3 address.
+ *   2. Read back the value and verify it is equal to the value written in Step 1.
+ *   3. If yes, fail the test.
+ *   4. Otherwise, pass the test.
  */
+void program_UDS_FE_secret_prod_partition() {
+    // Set AXI user ID to 1.
+    VPRINTF(LOW, "INFO: Granting MCU access to fuse controller...\n");
+    grant_caliptra_core_for_fc_writes();
 
 
-void secret_partition_zeroization(void) {
-  const uint32_t base_address = partitions[VENDOR_SECRET_PROD_PARTITION].address;
-  const uint32_t fuse_address = base_address;//CPTRA_SS_TEST_EXIT_TO_MANUF_TOKEN;
+    partition_t hw_part_uds = partitions[SECRET_MANUF_PARTITION];
+    partition_t hw_part_fe0 = partitions[SECRET_PROD_PARTITION_0];
+    partition_t hw_part_fe3 = partitions[SECRET_PROD_PARTITION_3];
 
-  const uint32_t data[4] = {0xdeadbeef, 0xcafebabe, 0x12345678, 0xabababab};
-  uint32_t read_data[4];
-  uint32_t zero_r_data[2];
-  uint32_t zeroize_num_times = 3;
+    uint32_t uds_base_address = hw_part_uds.address;
+    uint32_t fe0_base_address = hw_part_fe0.address;
+    uint32_t fe3_last_address = hw_part_fe3.address;
 
-  // Step 1. Write a full transition token into the partition.
-  dai_wr(fuse_address, data[0], data[1], 64, 0);
-  dai_wr(fuse_address+8, data[2], data[3], 64, 0);
+    uint32_t data[2] = { 0xA5A5A5A5, 0x5A5A5A5A };
+    uint32_t uds_read_data[2];
+    uint32_t fe0_read_data[2];
+    uint32_t fe3_read_data[2];
+    uint32_t read_data[2];
+    dai_rd(uds_base_address, &uds_read_data[0], &uds_read_data[1], 64, 0);
+    dai_rd(fe0_base_address, &fe0_read_data[0], &fe0_read_data[1], 64, 0);
+    dai_rd(fe3_last_address, &fe3_read_data[0], &fe3_read_data[1], 64, 0);
 
+    // Step 1
+    dai_wr(uds_base_address, data[0], data[1], 64, OTP_CTRL_STATUS_DAI_ERROR_MASK);
+    dai_wr(fe0_base_address, data[0], data[1], 64, OTP_CTRL_STATUS_DAI_ERROR_MASK);
+    dai_wr(fe3_last_address, data[0], data[1], 64, OTP_CTRL_STATUS_DAI_ERROR_MASK);
 
-  // Step 2. Read back the token and verify it is equal to the token written in Step 1.
-  //         This works as long as the secret partition has not been locked.
-  dai_rd(fuse_address, &read_data[0], &read_data[1], 64, 0);
-  dai_rd(fuse_address+8, &read_data[2], &read_data[3], 64, 0);
-  for (uint32_t idx = 0; idx < 4; idx++) {
-    if (data[idx] != read_data[idx]) {
-      VPRINTF(LOW, "ERROR: incorrect fuse data[index=%0d]: expected: %08X actual: %08X\n", idx, data[idx], read_data[idx]);
-      goto epilogue;
+    // Step 2, 3, 4
+    dai_rd(uds_base_address, &read_data[0], &read_data[1], 64, 0);
+    if (uds_read_data[0]!= read_data[0] || uds_read_data[1] != read_data[1]) {
+        VPRINTF(LOW, "ERROR: incorrect fuse data: expected: %08X actual: %08X\n", uds_read_data[0], read_data[0]);
+        VPRINTF(LOW, "ERROR: incorrect fuse data: expected: %08X actual: %08X\n", uds_read_data[1], read_data[1]);
+        handle_error("Data mismatch detected when reading UDS fuse in debug mode");
     }
-  }
-
-  // Step 3. Verify that the partition's digest register is 0.
-  uint32_t digest[2];
-  digest[0] = lsu_read_32(SOC_OTP_CTRL_VENDOR_SECRET_PROD_PARTITION_DIGEST_DIGEST_0);
-  VPRINTF(LOW, "Afte step3\n");
-  digest[1] = lsu_read_32(SOC_OTP_CTRL_VENDOR_SECRET_PROD_PARTITION_DIGEST_DIGEST_1);
-  if (digest[0] != 0 || digest[1] != 0) {
-    VPRINTF(LOW, "ERROR: digest is not 0\n");
-    goto epilogue;
-  }
-
-  // Step 4. Lock the partition by calculating a hardware digest.
-  calculate_digest(base_address, 0);
-
-  // Step 5. Reset the RTL.
-  reset_fc_lcc_rtl();
-  wait_dai_op_idle(0);
-
-  // Step 6. Try to read the token and verify that it results in an error as the
-  //         partition is secret and locked, thus not accessible by software anymore.
-  dai_rd(fuse_address, &read_data[0], &read_data[1], 64, OTP_CTRL_STATUS_DAI_ERROR_MASK);
-
-  // Step 7. Try to write a value into the partition and verify that it results in an error
-  //         as the partition has been locked in Step 5.
-  dai_wr(fuse_address, data[0], data[1], 64, OTP_CTRL_STATUS_DAI_ERROR_MASK);
-
-  // Step 8. Read back the digest from the partition's digest register and verify it
-  //         is non-zero.
-  digest[0] = lsu_read_32(SOC_OTP_CTRL_VENDOR_SECRET_PROD_PARTITION_DIGEST_DIGEST_0);
-  digest[1] = lsu_read_32(SOC_OTP_CTRL_VENDOR_SECRET_PROD_PARTITION_DIGEST_DIGEST_1);
-  if (digest[0] == 0 || digest[1] == 0) {
-    VPRINTF(LOW, "ERROR: digest is 0 - digest[0]=%0d, digest[1]=%0d\n", digest[0], digest[1]);
-    goto epilogue;
-  }
-
-  // Step 9. Apply zeroization multiple_times
-  for(uint32_t num_times = 0; num_times < zeroize_num_times; num_times++) {
-    // Zeroize fuse.
-    for (uint32_t addr = partitions[VENDOR_SECRET_PROD_PARTITION].address; addr < partitions[VENDOR_SECRET_PROD_PARTITION].digest_address; addr += (partitions[VENDOR_SECRET_PROD_PARTITION].granularity / 8)) {
-      dai_zer(addr, &read_data[0], &read_data[1], partitions[VENDOR_SECRET_PROD_PARTITION].granularity, 0);
-      if (read_data[0] != 0xFFFFFFFF || (partitions[VENDOR_SECRET_PROD_PARTITION].granularity> 32 && read_data[1] != 0xFFFFFFFF)) {
-        VPRINTF(LOW, "ERROR: [partitions[SECRET_LC_TRANSITION_PARTITION] ]fuse is not zeroized\n");
-        goto epilogue;
-      }
+    dai_rd(fe0_base_address, &read_data[0], &read_data[1], 64, 0);
+    if (fe0_read_data[0]!= read_data[0] || fe0_read_data[1] != read_data[1]) {
+        VPRINTF(LOW, "ERROR: incorrect fuse data: expected: %08X actual: %08X\n", fe0_read_data[0], read_data[0]);
+        VPRINTF(LOW, "ERROR: incorrect fuse data: expected: %08X actual: %08X\n", fe0_read_data[1], read_data[1]);
+        handle_error("Data mismatch detected when reading FE0 fuse in debug mode");
     }
-    memset(read_data, 0, sizeof(read_data));
-    // Zeroize marker field.
-    dai_zer(partitions[VENDOR_SECRET_PROD_PARTITION].zer_address, &read_data[0], &read_data[1], 64, 0);
-    if (read_data[0] != 0xFFFFFFFF || read_data[1] != 0xFFFFFFFF) {
-      VPRINTF(LOW, "ERROR: [partitions[VENDOR_SECRET_PROD_PARTITION] marker is not zeroized\n");
-      goto epilogue;
+    dai_rd(fe3_last_address, &read_data[0], &read_data[1], 64, 0);
+    if (fe3_read_data[0]!= read_data[0] || fe3_read_data[1] != read_data[1]) {
+        VPRINTF(LOW, "ERROR: incorrect fuse data: expected: %08X actual: %08X\n", fe3_read_data[0], read_data[0]);
+        VPRINTF(LOW, "ERROR: incorrect fuse data: expected: %08X actual: %08X\n", fe3_read_data[1], read_data[1]);
+        handle_error("Data mismatch detected when reading FE3 fuse in debug mode");
     }
-    memset(read_data, 0, sizeof(read_data));
-    // Zeroize digest field.
-    dai_zer(partitions[VENDOR_SECRET_PROD_PARTITION].digest_address, &read_data[0], &read_data[1], 64, 0);
-    if (read_data[0] != 0xFFFFFFFF || read_data[1] != 0xFFFFFFFF) {
-      VPRINTF(LOW, "ERROR: [partitions[VENDOR_SECRET_PROD_PARTITION] digest is not zeroized\n");
-      goto epilogue;
-    }
-    memset(read_data, 0, sizeof(read_data));
-  }
 
- epilogue:
-  VPRINTF(LOW, "secret_partition_zeroization test finished\n");
+
+    VPRINTF(LOW, "DEBUG: UDS/FE programming rules are applied successfully\n");
 }
 
 void main (void) {
     VPRINTF(LOW, "=================\nMCU Caliptra Boot Go\n=================\n\n")
-
+    
     mcu_cptra_init_d();
     wait_dai_op_idle(0);
-
+    
     lcc_initialization();
-    grant_mcu_for_fc_writes();
 
-
-    transition_state_check(TEST_UNLOCKED0, raw_unlock_token[0], raw_unlock_token[1], raw_unlock_token[2], raw_unlock_token[3], 1);
-
-    initialize_otp_controller();
-    VPRINTF(LOW, "=================\nSecret_partition_zeroization\n=================\n\n")
-    secret_partition_zeroization();
-
-    for (uint8_t ii = 0; ii < 160; ii++) {
-        __asm__ volatile ("nop"); // Sleep loop as "nop"
+    uint32_t lc_state_curr = read_lc_state();
+    if (lc_state_curr != 1) {
+        handle_error("ERROR: LCC is not initialized from TEST_UNLOCKED0\n");
     }
+
+    program_UDS_FE_secret_prod_partition();
+
+    mcu_sleep(160);
+
 
     SEND_STDOUT_CTRL(0xff);
 }
