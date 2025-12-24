d62f51d01180919faa023f1813017e81cbcbf5c6
EMRE KARABULUT <63821295+ekarabu@users.noreply.github.com>
2025-12-23 19:37:02 -0500

    [TB] Updates FC Interrupt Test with Reset (#997)
    * Resolves #942
    
    * seperated reg bit for comparison
    
    * MICROSOFT AUTOMATED PIPELINE: Stamp 'user/ekarabulut/fc_interrupt' with updated timestamp and hash after successful run

--- src/integration/test_suites/caliptra_ss_fuse_ctrl_interrupts/caliptra_ss_fuse_ctrl_interrupts.c
+++ src/integration/test_suites/caliptra_ss_fuse_ctrl_interrupts/caliptra_ss_fuse_ctrl_interrupts.c
@@ -41,64 +41,79 @@ void main (void) {
     
     mcu_cptra_init_d();
     wait_dai_op_idle(0);
+
+    uint32_t alert_state, operation_done, otp_error;
       
-    lcc_initialization();
-    grant_caliptra_core_for_fc_writes(); 
-
-    transition_state(TEST_UNLOCKED0, raw_unlock_token, false);
-    wait_dai_op_idle(0);
-
     initialize_otp_controller();
 
     // Enable `otp_operation_done` and `otp_error` interrupts.
     lsu_write_32(SOC_OTP_CTRL_INTERRUPT_ENABLE, 0x3);
 
-    /*
-     * 1: An ordinary, successful DAI operation must result in an `otp_operation_done` interrupt.
-     */
-
-    dai_wr(CPTRA_SS_MANUF_DEBUG_UNLOCK_TOKEN, 0x1, 0, 32, 0);
-
-    uint32_t alert_state = lsu_read_32(SOC_OTP_CTRL_INTERRUPT_STATE);
-    if (((alert_state >> OTP_CTRL_INTERRUPT_STATE_OTP_OPERATION_DONE_LOW) & 0x1 != 0x1) ||
-        ((alert_state >> OTP_CTRL_INTERRUPT_STATE_OTP_ERROR_LOW) & 0x1 != 0x0)) {
-        VPRINTF(LOW, "ERROR: wrong interrupt signaled\n");
-        goto epilogue;
-    }
-    lsu_write_32(SOC_OTP_CTRL_INTERRUPT_STATE, 0x3);
 
     /*
-     * 2: An invalid DAI operation must result in an `otp_error` interrupt.
-     */
-
-    grant_mcu_for_fc_writes();
-    dai_wr(CPTRA_CORE_UDS_SEED, 0x1, 0x1, 64, OTP_CTRL_STATUS_DAI_ERROR_MASK); // invalid AXI ID
-
-    alert_state = lsu_read_32(SOC_OTP_CTRL_INTERRUPT_STATE);
-    if (((alert_state >> OTP_CTRL_INTERRUPT_STATE_OTP_OPERATION_DONE_LOW) & 0x1 != 0x1) ||
-        ((alert_state >> OTP_CTRL_INTERRUPT_STATE_OTP_ERROR_LOW) & 0x1 != 0x1)) {
-        VPRINTF(LOW, "ERROR: wrong interrupt signaled\n");
-        goto epilogue;
-    }
-    lsu_write_32(SOC_OTP_CTRL_INTERRUPT_STATE, 0x3);
-
-
-    /*
-     * 3: Sanity check: triggering interrupts through the `INTR_TEST` register.
+     * 1: Sanity check: triggering interrupts through the `INTR_TEST` register.
      */
 
     lsu_write_32(SOC_OTP_CTRL_INTERRUPT_TEST, 0x3);
     
     alert_state = lsu_read_32(SOC_OTP_CTRL_INTERRUPT_STATE);
-    if (((alert_state >> OTP_CTRL_INTERRUPT_STATE_OTP_OPERATION_DONE_LOW) & 0x1 != 0x1) ||
-        ((alert_state >> OTP_CTRL_INTERRUPT_STATE_OTP_ERROR_LOW) & 0x1 != 0x1)) {
-        VPRINTF(LOW, "ERROR: wrong interrupt signaled\n");
+
+    operation_done = (alert_state >> OTP_CTRL_INTERRUPT_STATE_OTP_OPERATION_DONE_LOW) & 0x1;
+    otp_error = (alert_state >> OTP_CTRL_INTERRUPT_STATE_OTP_ERROR_LOW) & 0x1;
+
+    if (operation_done != 0x1 && otp_error != 0x1) {
+        handle_error("ERROR: INTERRUPT_TEST register does not work as expected.\n");
     }
 
-epilogue:
-    for (uint8_t i = 0; i < 160; i++) {
-        __asm__ volatile ("nop"); // Sleep loop as "nop"
+    reset_fc_lcc_rtl();
+    wait_dai_op_idle(0);
+    // Enable `otp_operation_done` and `otp_error` interrupts.
+    lsu_write_32(SOC_OTP_CTRL_INTERRUPT_ENABLE, 0x3);
+    alert_state = lsu_read_32(SOC_OTP_CTRL_INTERRUPT_STATE); // Read back to ensure clear
+    if (alert_state != 0x0) {
+        handle_error("ERROR: interrupt state register was not cleared properly.\n");
     }
 
+    /*
+     * 2: An ordinary, successful DAI operation must result in an `otp_operation_done` interrupt.
+     */
+
+    dai_wr(CPTRA_SS_MANUF_DEBUG_UNLOCK_TOKEN, 0x1, 0, 32, 0);
+
+    alert_state = lsu_read_32(SOC_OTP_CTRL_INTERRUPT_STATE);
+
+    operation_done = (alert_state >> OTP_CTRL_INTERRUPT_STATE_OTP_OPERATION_DONE_LOW) & 0x1;
+    otp_error = (alert_state >> OTP_CTRL_INTERRUPT_STATE_OTP_ERROR_LOW) & 0x1;
+
+    if (operation_done != 0x1 && otp_error != 0x0) {
+        handle_error("ERROR: wrong interrupt signaled\n");
+    }
+
+
+    reset_fc_lcc_rtl();
+    wait_dai_op_idle(0);
+    // Enable `otp_operation_done` and `otp_error` interrupts.
+    lsu_write_32(SOC_OTP_CTRL_INTERRUPT_ENABLE, 0x3);
+    alert_state = lsu_read_32(SOC_OTP_CTRL_INTERRUPT_STATE); // Read back to ensure clear
+    if (alert_state != 0x0) {
+        handle_error("ERROR: interrupt state register was not cleared properly.\n");
+    }
+
+    /*
+     * 3: An invalid DAI operation must result in an `otp_error` interrupt.
+     */
+
+
+    dai_wr(CPTRA_CORE_UDS_SEED, 0x1, 0x1, 64, OTP_CTRL_STATUS_DAI_ERROR_MASK); // invalid AXI ID
+
+    alert_state = lsu_read_32(SOC_OTP_CTRL_INTERRUPT_STATE);
+    operation_done = (alert_state >> OTP_CTRL_INTERRUPT_STATE_OTP_OPERATION_DONE_LOW) & 0x1;
+    otp_error = (alert_state >> OTP_CTRL_INTERRUPT_STATE_OTP_ERROR_LOW) & 0x1;
+
+    if (operation_done != 0x0 && otp_error != 0x1) {
+        handle_error("ERROR: error indication signal was not asserted.\n");
+    }
+
+    VPRINTF(LOW, "INFO: All interrupt tests passed successfully.\n");
     SEND_STDOUT_CTRL(0xff);
 }
