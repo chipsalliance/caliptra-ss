68371e612e6ebf9ee9c63617793e440d77975116
Caleb Whitehead <cwhitehead@microsoft.com>
2026-08-22 10:17:35 -0700

    usb2 submodule: remove vestigial AHB sync logic to OCP rec

--- src/integration/testbench/uvm/usb/caliptra_ss_usb_test_pkg.sv
+++ src/integration/testbench/uvm/usb/caliptra_ss_usb_test_pkg.sv
@@ -16,7 +16,7 @@
 package caliptra_ss_usb_test_pkg;
 
   // Force VCS recompile when USB UVM package contents change.
-  localparam int USB_PKG_VERSION = 176;
+  localparam int USB_PKG_VERSION = 178;
 
   import uvm_pkg::*;
   `include "uvm_macros.svh"
